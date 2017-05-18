9cf1be8f039836e22232ce91700b0c94426077e4
jofemodo <fernando@zynthian.org>
2017-05-18 14:46:50 +0200

    + zynthian.sh: Include a custom initialization script for X11 system. It can be used, for instance, for configuring screen rotation with xinit. + zynthian_gui.py: Adjust some sizes in controllers.

--- zynthian.sh
+++ zynthian.sh
@@ -23,6 +23,7 @@
 #******************************************************************************
 
 source "/zynthian/zynthian-sys/scripts/zynthian_envars.sh"
+source "/etc/X11/zynthian"
 
 #export ZYNTHIAN_LOG_LEVEL=10			# 10=DEBUG, 20=INFO, 30=WARNING, 40=ERROR, 50=CRITICAL
 #export ZYNTHIAN_RAISE_EXCEPTIONS=0
