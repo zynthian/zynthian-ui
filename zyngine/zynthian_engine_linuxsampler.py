4a2c45d288b0382928104f7e68295a2bfbeaddf0
jofemodo <fernando@zynthian.org>
2017-08-03 00:37:24 +0200

    Restore line from @Schpion that was deleted incorrectly from the pull request.

--- zyngine/zynthian_engine_linuxsampler.py
+++ zyngine/zynthian_engine_linuxsampler.py
@@ -355,6 +355,8 @@ class zynthian_engine_linuxsampler(zynthian_engine):
 			fx_ctrl_i=parts[1]
 			logging.debug("LSCP: Sending controller %s => %s" % (zctrl.name,zctrl.value))
 			self.lscp_send_single("SET EFFECT_INSTANCE_INPUT_CONTROL VALUE %s %s %s" % (fx_id,fx_ctrl_i,zctrl.value))
+		else:
+			super().send_controller_value(zctrl)
 
 	# ---------------------------------------------------------------------------
 	# Specific functions
