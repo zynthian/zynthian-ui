93f6a32d56f9af48b29439b98d3fabc7814c761f
jofemodo <fernando@zynthian.org>
2018-11-29 18:29:38 +0100

    After preloading presets, force set_engine when selecting the current preset from the list.

--- zyngine/zynthian_layer.py
+++ zyngine/zynthian_layer.py
@@ -178,13 +178,19 @@ class zynthian_layer:
 			self.preset_name=self.preset_list[i][2]
 			self.preset_info=copy.deepcopy(self.preset_list[i])
 			self.preset_bank_index=self.bank_index
-			self.preload_index=i
-			self.preload_name=self.preset_name
-			self.preload_info=self.preset_info
+			
+			if self.preload_info and not self.engine.cmp_presets(self.preload_info,self.preset_info):
+				force_set_engine=True
+				self.preload_index=i
+				self.preload_name=self.preset_name
+				self.preload_info=self.preset_info
+			else:
+				force_set_engine=False
+
 			logging.info("Preset Selected: %s (%d)" % (self.preset_name,i))
 			#=> '+self.preset_list[i][3]
 	
-			if set_engine and (last_preset_index!=i or not last_preset_name):
+			if force_set_engine or (set_engine and (last_preset_index!=i or not last_preset_name)):
 				#TODO => Review this!!
 				#self.load_ctrl_config()
 				return self.engine.set_preset(self, self.preset_info)
