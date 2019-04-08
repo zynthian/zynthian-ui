e0c85d235b174d03d0f96e0f993dbde2a38e8f6f
riban <brian@riban.co.uk>
2019-04-08 22:15:16 +0100

    Adds riban I2C HWC

--- zyngui/zynthian_gui_config.py
+++ zyngui/zynthian_gui_config.py
@@ -134,6 +134,11 @@ elif wiring_layout=="MCP23017_EXTRA":
 	if not zyncoder_pin_b: zyncoder_pin_b=[101,104,109,112]
 	if not zynswitch_pin: zynswitch_pin=[100,103,108,111,106,107,114,115]
 	select_ctrl=3
+elif wiring_layout=="I2C_HWC":
+	if not zyncoder_pin_a: zyncoder_pin_a=[1,2,3,4]
+	zyncoder_pin_b=[0,0,0,0]
+	if not zynswitch_pin: zynswitch_pin=[1,2,3,4]
+	select_ctrl=3
 
 # Desktop Development & Emulation
 elif wiring_layout=="EMULATOR":
