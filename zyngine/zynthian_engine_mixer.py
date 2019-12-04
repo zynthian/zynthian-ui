9c468f374b3497574368313ed5aedc2e36ed16d9
jofemodo <fernando@zynthian.org>
2019-12-04 20:04:56 +0100

    Refactorize get_controllers_dict(), adding support for selectors, etc.

--- zyngine/zynthian_engine_mixer.py
+++ zyngine/zynthian_engine_mixer.py
@@ -107,24 +107,90 @@ class zynthian_engine_mixer(zynthian_engine):
 	#----------------------------------------------------------------------------
 
 	def get_controllers_dict(self, layer):
-		zctrls=OrderedDict()
-		ctrl_name = None
-		ctrl_symbol = None
-		ctrl_type = None
-		ctrl_value = 50
-		ctrl_maxval = 100
-		ctrl_minval = 0
+		zctrls = {}
 
-		logging.debug("MIXER CTRL LIST: ".format(self.ctrl_list))
+		logging.debug("MIXER CTRL LIST: {}".format(self.ctrl_list))
 
 		try:
-			for byteLine in check_output("amixer -M -c {}".format(self.device_name), shell=True).splitlines():
-				line = byteLine.decode("utf-8")
+			ctrls = check_output("amixer -M -c {}".format(self.device_name), shell=True).decode("utf-8").split("Simple mixer control ")
+			for ctrl in ctrls:
+				lines = ctrl.splitlines()
+				if len(lines)==0:
+					continue
 
-				if line.find('Simple mixer control')>=0:
-					if ctrl_name and ctrl_type and (not self.ctrl_list or ctrl_symbol in self.ctrl_list):
+				m = re.match("'(.*?)'.*", lines[0], re.M | re.I)
+				if m:
+					ctrl_name = m.group(1).strip()
+					ctrl_symbol = ctrl_name.replace(' ', '_')
+				else:
+					ctrl_name = None
+					ctrl_symbol = None
+	
+				ctrl_type = None
+				ctrl_chans = None
+				ctrl_items = None
+				ctrl_item0 = None
+				ctrl_value = 50
+				ctrl_maxval = 100
+				ctrl_minval = 0
+
+				logging.debug("MIXER CONTROL => {}\n{}".format(ctrl_name, ctrl))
+				for line in lines[1:]:
+					try:
+						key, value = line.strip().split(": ",1)
+						logging.debug("  {} => {}".format(key,value))
+					except:
+						continue
+
+					if key=='Playback channels':
+						ctrl_type = "Playback"
+						#ctrl_name = "Volume"
+						ctrl_chans = value.strip().split(' - ')
+
+					elif key=='Capture channels':
+						ctrl_type = "Capture"
+						#ctrl_name = "Input Gain"
+						ctrl_chans = value.strip().split(' - ')
+
+					elif key=='Capabilities' and value=='enum':
+						ctrl_type = "Selector"
+
+					elif key=='Items':
+						ctrl_items = value[1:-1].split("' '")
+
+					elif key=='Item0':
+						ctrl_item0 = value[1:-1]
+
+					elif ctrl_chans and key in ctrl_chans:
+						m = re.match(".*(Playback|Capture).*\[(\d*)%\].*", value, re.M | re.I)
+						if m:
+							ctrl_value = int(m.group(2))
+							if m.group(1) == 'Capture':
+								ctrl_type = "Capture"
+							else:
+								ctrl_type = "Playback"
+						else:
+							m = re.match(".*\[(\d*)%\].*", line, re.M | re.I)
+							if m:
+								ctrl_value = int(m.group(1))
+
+				if ctrl_symbol and ctrl_type and (not self.ctrl_list or ctrl_name in self.ctrl_list):
+					if ctrl_type=='Selector' and len(ctrl_items)>1:
+						logging.debug("ADDING ZCTRL SELECTOR: {} => {} ({})".format(ctrl_symbol, ctrl_item0, ctrl_value))
 						zctrls[ctrl_symbol]=zynthian_controller(self, ctrl_symbol, ctrl_name, {
-							'graph_path': "'{}' {}".format(ctrl_symbol, ctrl_type),
+							'graph_path': "'{}'".format(ctrl_name),
+							'labels': ctrl_items,
+							'ticks': list(range(len(ctrl_items))),
+							'value': ctrl_item0,
+							'value_min': 0,
+							'value_max': len(ctrl_items)-1,
+							'is_toggle': False,
+							'is_integer': True
+						})
+					elif ctrl_type in ("Playback" ,"Capture"):
+						logging.debug("ADDING ZCTRL LEVEL: {} => {}".format(ctrl_symbol, ctrl_value))
+						zctrls[ctrl_symbol]=zynthian_controller(self, ctrl_symbol, ctrl_name, {
+							'graph_path': "'{}' {}".format(ctrl_name, ctrl_type),
 							'value': ctrl_value,
 							'value_default': 50,
 							'value_min': ctrl_minval,
@@ -133,61 +199,28 @@ class zynthian_engine_mixer(zynthian_engine):
 							'is_integer': True
 						})
 
-					m = re.match("Simple mixer control '(.*?)'.*", line, re.M | re.I)
-					if m:
-						ctrl_name = m.group(1).strip()
-						ctrl_symbol = ctrl_name.replace(' ', '_')
-					else:
-						ctrl_name = None
-						ctrl_symbol = None
-
-					ctrl_type = None
-					ctrl_value = 50
-
-
-				elif line.find('Playback channels:')>=0:
-					ctrl_type = "Playback"
-					ctrl_name = "Volume"
-
-				elif line.find('Capture channels:')>=0:
-					ctrl_type = "Capture"
-					ctrl_name = "Input Gain"
-
-				else:
-					m = re.match(".*(Playback|Capture).*\[(\d*)%\].*", line, re.M | re.I)
-					if m:
-						ctrl_value = int(m.group(2))
-						if m.group(1) == 'Capture':
-							ctrl_type = "Capture"
-						else:
-							ctrl_type = "Playback"
-					else:
-						m = re.match(".*\[(\d*)%\].*", line, re.M | re.I)
-						if m:
-							ctrl_value = int(m.group(1))
-
-			if ctrl_name and ctrl_type and (not self.ctrl_list or ctrl_symbol in self.ctrl_list):
-				zctrls[ctrl_symbol]=zynthian_controller(self, ctrl_symbol, ctrl_name, {
-					'graph_path': "'{}' {}".format(ctrl_symbol, ctrl_type),
-					'value': ctrl_value,
-					'value_default': 50,
-					'value_min': ctrl_minval,
-					'value_max': ctrl_maxval,
-					'is_toggle': False,
-					'is_integer': True
-				})
-
 		except Exception as err:
 			logging.error(err)
 
-		self.generate_ctrl_screens(zctrls)
+		# Sort zctrls to match the configured mixer control list
+		sorted_zctrls = OrderedDict()
+		for ctrl_name in self.ctrl_list:
+			ctrl_symbol = ctrl_name.replace(' ', '_')
+			sorted_zctrls[ctrl_symbol] = zctrls[ctrl_symbol]
 
-		return zctrls
+		# Generate control screens
+		self.generate_ctrl_screens(sorted_zctrls)
+
+		return sorted_zctrls
 
 
 	def send_controller_value(self, zctrl):
 		try:
-			amixer_command = "amixer -M -c {} set {} {}% unmute".format(self.device_name, zctrl.graph_path, zctrl.value)
+			if zctrl.labels:
+				amixer_command = "amixer -M -c {} set {} '{}'".format(self.device_name, zctrl.graph_path, zctrl.get_value2label())
+			else:
+				amixer_command = "amixer -M -c {} set {} {}% unmute".format(self.device_name, zctrl.graph_path, zctrl.value)
+
 			logging.debug(amixer_command)
 			check_output(amixer_command, shell=True)
 
