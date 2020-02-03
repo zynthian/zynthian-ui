c88552f24c6b1e6cea56551ff4b584abac55111a
jofemodo <fernando@zynthian.org>
2020-02-03 01:41:11 +0100

    Fix RTP-MIDI state on snapshot restoring

--- zynthian_gui.py
+++ zynthian_gui.py
@@ -199,6 +199,7 @@ class zynthian_gui:
 
 	def init_midi_services(self):
 		#Start / stop MIDI aux. services
+		self.screens['admin'].default_rtpmidi()
 		self.screens['admin'].default_qmidinet()
 		self.screens['admin'].default_touchosc()
 		self.screens['admin'].default_aubionotes()
