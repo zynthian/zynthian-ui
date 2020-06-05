8dc72e275322cf6bc6ed13e73b0ebd252938e057
Jan Ypma <jan@ypmania.nl>
2020-06-05 11:22:47 +0100

    Show class for LV2 effects when selecting
    This provides a convenient sorting and better discoverability for LV2
    effects, especially if more than a few are enabled from the web UI.
    
    Configured plugins are grouped by categories and then sorted. The
    categories are displayed as non-selectable headers in a different
    color.
    
    The built-in synths are still displayed first in their original order.

--- zyngui/zynthian_gui_engine.py
+++ zyngui/zynthian_gui_engine.py
@@ -62,7 +62,10 @@ class zynthian_gui_engine(zynthian_gui_selector):
 			["FS", ("FluidSynth", "FluidSynth - SF2 Player", "MIDI Synth", zynthian_engine_fluidsynth, True)],
 			["LS", ("LinuxSampler", "LinuxSampler - SFZ/GIG Player", "MIDI Synth", zynthian_engine_linuxsampler, True)],
 			["BF", ("setBfree", "setBfree - Hammond Emulator", "MIDI Synth", zynthian_engine_setbfree, True)],
-			["AE", ("Aeolus", "Aeolus - Pipe Organ Emulator", "MIDI Synth", zynthian_engine_aeolus, True)]
+			["AE", ("Aeolus", "Aeolus - Pipe Organ Emulator", "MIDI Synth", zynthian_engine_aeolus, True)],
+			['PD', ("PureData", "PureData - Visual Programming", "Special", zynthian_engine_puredata, True)],
+			['CS', ("CSound", "CSound Audio Language", "Special", zynthian_engine_csound, False)],
+			['MD', ("MOD-UI", "MOD-UI - Plugin Host", "Special", zynthian_engine_modui, True)]
 		])
 
 		if check_pianoteq_binary():
@@ -73,13 +76,21 @@ class zynthian_gui_engine(zynthian_gui_selector):
 				" (Demo)" if PIANOTEQ_TRIAL else "")
 			cls.engine_info['PT'] = (PIANOTEQ_NAME, pianoteq_title, "MIDI Synth", zynthian_engine_pianoteq, True)
 
+		builtin = {}
+		for en, info in cls.engine_info.items():
+			builtin[en] = info
+		
+		cls.engine_info_categorized = {}
+		if len(builtin) > 0:
+			cls.engine_info_categorized["Zynthian Built-in"] = builtin
+		
 		for plugin_name, plugin_info in get_jalv_plugins().items():
-			cls.engine_info['JV/{}'.format(plugin_name)] = (plugin_name, "{} - Plugin LV2".format(plugin_name), plugin_info['TYPE'], zynthian_engine_jalv, plugin_info['ENABLED'])
-
-		cls.engine_info['PD'] = ("PureData", "PureData - Visual Programming", "Special", zynthian_engine_puredata, True)
-		cls.engine_info['CS'] = ("CSound", "CSound Audio Language", "Special", zynthian_engine_csound, False)
-		cls.engine_info['MD'] = ("MOD-UI", "MOD-UI - Plugin Host", "Special", zynthian_engine_modui, True)
-
+			en = 'JV/{}'.format(plugin_name)
+			info = (plugin_name, plugin_name, plugin_info['TYPE'], zynthian_engine_jalv, plugin_info['ENABLED'])
+			pluginClass = plugin_info.get('CLASS','')
+			cls.engine_info[en] = info
+			category = cls.engine_info_categorized.setdefault("LV2 {}".format(pluginClass), {})
+			category[en] = info
 
 	def __init__(self):
 		self.zyngine_counter = 0
@@ -92,37 +103,70 @@ class zynthian_gui_engine(zynthian_gui_selector):
 		self.engine_type = etype
 		self.midi_chan = None
 
-
 	def set_fxchain_mode(self, midi_chan):
 		self.engine_type = "Audio Effect"
 		self.midi_chan = midi_chan
 
-
 	def set_midichain_mode(self, midi_chan):
 		self.engine_type = "MIDI Tool"
 		self.midi_chan = midi_chan
+		self.init_engine_info()
 
+	def fill_list_item(self, en, info):
+		# For some engines, check if needed channels are free ...
+		if (en in self.check_channels_engines and
+			not all(chan in self.zyngui.screens['layer'].get_free_midi_chans() for chan in info[3].get_needed_channels())):
+			return
 
+		ei=self.engine_info[en]
+		self.list_data.append((en,len(self.list_data),ei[1],ei[0]))
+
+	def filtered_categories(self):
+		result = {}
+		for cat, infos in self.engine_info_categorized.items():
+			result_infos = {}
+			for en, info in infos.items():
+				if (info[4] and (info[2]==self.engine_type or self.engine_type is None) and
+				    (en not in self.single_layer_engines or en not in self.zyngines)):
+					result_infos[en] = info
+			if len(result_infos) > 0:
+				result[cat] = result_infos
+		return result
+                
 	def fill_list(self):
 		self.init_engine_info()
-		self.index=0
 		self.list_data=[]
-		i=0
-		for en, info in self.engine_info.items():
-			if (info[4] and (info[2]==self.engine_type or self.engine_type is None) and
-				(en not in self.single_layer_engines or en not in self.zyngines)):
 
-				# For some engines, check if needed channels are free ...
-				if (en in self.check_channels_engines and
-					not all(chan in self.zyngui.screens['layer'].get_free_midi_chans() for chan in info[3].get_needed_channels())):
-						continue
+		# Sort category headings, but headings starting with "Zynthian" are shown first
+		for cat, infos in sorted(filtered_categories(self).items(), key = lambda kv:
+			                 "! {}".format(kv[0]) if kv[0].startswith("Zynthian") else kv[0]):
+			self.list_data.append((None,len(self.list_data),"  {}".format(cat)))
+			# Sort the list of plugins, unless it's the built-in list (which has a fixed order)
+			if cat.startswith("Zynthian"):
+				items = infos.items()
+			else:
+				items = sorted(infos.items(), key = lambda kv: kv[0])
+			for en, info in items:
+				self.fill_list_item(en, info)
 
-				ei=self.engine_info[en]
-				self.list_data.append((en,i,ei[1],ei[0]))
-				i=i+1
+		if (len(self.list_data) == 0):
+			self.list_data.append((None,len(self.list_data),"http://{}.local/ to enable".format(os.uname().nodename)))
 
+		# Select the first element that is not a category heading
+		self.index=0
+		for idx, val in enumerate(self.list_data):
+			if val[0] != None:
+				self.index = idx
+				break
+		
 		super().fill_list()
 
+	def fill_listbox(self):
+		super().fill_listbox()
+		# TODO Give the category headers a nice color
+		for idx, val in enumerate(self.list_data):
+			if (val[0] == None):
+				self.listbox.itemconfig(idx, {'fg':zynthian_gui_config.color_tx_off,'bg':zynthian_gui_config.color_bg})
 
 	def select_action(self, i, t='S'):
 		self.zyngui.screens['layer'].add_layer_engine(self.start_engine(self.list_data[i][0]), self.midi_chan)
