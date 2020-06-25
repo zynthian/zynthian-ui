zynthian/zynthian-ui

commit b49696d449980b523488a82d70e0fc718c44811c

Inprove zynthian_gui_controller: selector mode & float numbers

zyngui/zynthian_gui_controller.py
@@ -55,6 +55,7 @@ class zynthian_gui_controller:
 		self.n_values=127
 		self.max_value=127
 		self.inverted=False
+		self.selmode = False
 		self.step=1
 		self.mult=1
 		self.val0=0
@@ -148,7 +149,11 @@ class zynthian_gui_controller:
 
 			try:
 				if self.zctrl.ticks:
-					if self.inverted:
+					if self.selmode:
+						i = val
+						valplot=self.scale_plot*val
+						val=self.zctrl.ticks[i]
+					elif self.inverted:
 						for i in reversed(range(self.n_values)):
 							if val<=self.zctrl.ticks[i]:
 								break
@@ -439,6 +444,7 @@ class zynthian_gui_controller:
 		self.value=None
 		self.n_values=127
 		self.inverted=False
+		self.selmode = False
 		self.scale_value=1
 		self.format_print=None
 		self.set_title(zctrl.short_name)
@@ -449,23 +455,29 @@ class zynthian_gui_controller:
 		#List of values (value selector)
 		if isinstance(zctrl.labels,list):
 			self.n_values=len(zctrl.labels)
-			val=zctrl.value-zctrl.value_min
 			if isinstance(zctrl.ticks,list):
 				if zctrl.ticks[0]>zctrl.ticks[-1]:
 					self.inverted=True
 				if (isinstance(zctrl.midi_cc, int) and zctrl.midi_cc>0):
 					self.max_value=127
 					self.step=max(1,int(16/self.n_values))
+					val=zctrl.value-zctrl.value_min
 				else:
-					if zctrl.value_range>32:
-						self.step = max(4,int(zctrl.value_range/(self.n_values*4)))
-						self.max_value = zctrl.value_range + self.step*4
-					else:
-						self.mult=max(4,int(32/self.n_values))
-						self.max_value = zctrl.value_range + 1
+					self.selmode = True
+					self.max_value = self.n_values-1
+					self.mult = max(4,int(32/self.n_values))
+					val=zctrl.get_value2index()
+
+					#if zctrl.value_range>32:
+						#self.step = max(4,int(zctrl.value_range/(self.n_values*4)))
+						#self.max_value = zctrl.value_range + self.step*4
+					#else:
+					#	self.mult=max(4,int(32/self.n_values))
+					#	self.max_value = zctrl.value_range + 1
 			else:
 				self.max_value=127;
 				self.step=max(1,int(16/self.n_values))
+				val=zctrl.value-zctrl.value_min
 
 		#Numeric value
 		else:
@@ -484,23 +496,31 @@ class zynthian_gui_controller:
 
 			else:
 				r=zctrl.value_max-zctrl.value_min
-				#Integer < 127
-				if isinstance(r,int) and r<=127:
-					self.max_value=self.n_values=r
-					self.mult=max(1,int(128/self.n_values))
-					val=zctrl.value-zctrl.value_min
-				#Integer > 127, not MIDI controller
-				elif isinstance(r,int) and zctrl.midi_cc is None:
-					self.max_value=self.n_values=r
-					self.scale_value=1
-					val=(zctrl.value-zctrl.value_min)
-				#Float or (Integer>127 and MIDI controller)
+				if isinstance(r,int):
+					#Integer < 127
+					if r<=127:
+						self.max_value=self.n_values=r
+						self.mult=max(1,int(128/self.n_values))
+						val=zctrl.value-zctrl.value_min
+					#Integer > 127
+					else:
+						#Not MIDI controller
+						if zctrl.midi_cc is None:
+							self.max_value=self.n_values=r
+							self.scale_value=1
+							val=(zctrl.value-zctrl.value_min)
+						#MIDI controller
+						else:
+							self.max_value=self.n_values=127
+							self.scale_value=r/self.max_value
+							val=(zctrl.value-zctrl.value_min)/self.scale_value
+				#Float
 				else:
-					self.max_value=self.n_values=127
+					self.max_value=self.n_values=200
 					self.scale_value=r/self.max_value
-					if self.scale_value<0.013:
+					if zctrl.value_min>-10 and zctrl.value_max<10:
 						self.format_print="{0:.2f}"
-					elif self.scale_value<0.13:
+					elif zctrl.value_min>-100 and zctrl.value_max<100:
 						self.format_print="{0:.1f}"
 					val=(zctrl.value-zctrl.value_min)/self.scale_value
 
@@ -509,7 +529,9 @@ class zynthian_gui_controller:
 					self.step=0
 
 		#Calculate scale parameter for plotting
-		if zctrl.ticks:
+		if self.selmode:
+			self.scale_plot=self.max_value/(self.n_values-1)
+		elif zctrl.ticks:
 			self.scale_plot=self.max_value/zctrl.value_range
 		elif self.n_values>1:
 			self.scale_plot=self.max_value/(self.n_values-1)
@@ -537,6 +559,8 @@ class zynthian_gui_controller:
 
 	def zctrl_sync(self):
 		#List of values (value selector)
+		if self.selmode:
+			val=self.zctrl.get_label2index()
 		if self.zctrl.labels:
 			#logging.debug("ZCTRL SYNC LABEL => {}".format(self.zctrl.get_value2label()))
 			val=self.zctrl.get_label2value(self.zctrl.get_value2label())

zyngine/zynthian_controller.py
@@ -278,7 +278,7 @@ class zynthian_controller:
 				logging.warning("Can't send controller feedback '{}' => Val={}".format(self.symbol,e))
 
 
-	def get_value2label(self, val=None):
+	def get_value2index(self, val=None):
 		if val is None:
 			val=self.value
 		try:
@@ -286,22 +286,30 @@ class zynthian_controller:
 				if self.ticks[0]>self.ticks[-1]:
 					for i in reversed(range(len(self.labels))):
 						if val<=self.ticks[i]:
-							return self.labels[i]
-					return self.labels[0]
+							return i
+					return 0
 				else:
 					for i in range(len(self.labels)-1):
 						#logging.debug("V2L testing range {} => {} in {}-{}".format(i,val,self.ticks[i],self.ticks[i+1]))
 						if val<self.ticks[i+1]:
-							return self.labels[i]
-					return self.labels[i+1]
+							return i
+					return i+1
 			elif self.labels:
 				i=min(int((val-self.value_min)*len(self.labels)/self.value_range), len(self.labels)-1)
 				#logging.debug("V2L => {} has index {}".format(val,i))
-				return self.labels[i]
+				return i
 			else:
-				return val
+				return None
 		except Exception as e:
 			logging.error(e)
+
+
+	def get_value2label(self, val=None):
+		i = self.get_value2index(val)
+		if i is not None:
+			return self.labels[i]
+		else:
+			return val
 
 
 	def get_label2value(self, label):

commit d3458888acce22dbcbe66dc406f49a384c53f1bd

Fix MIDI learning for value-based controllers
Commit #b49696d broke MIDI learning of value-based controllers, probably
because of a typo. Trying to MIDI learn a value-based controller results
in this "error" message in the log:

DEBUG:zynthian_controller: 'zynthian_controller' object has no attribute 'get_label2index'

This commit seems to fix that, by replacing the method call with what I
think was intended.

zyngui/zynthian_gui_controller.py
@@ -566,7 +566,7 @@ class zynthian_gui_controller:
 	def zctrl_sync(self):
 		#List of values (value selector)
 		if self.selmode:
-			val=self.zctrl.get_label2index()
+			val=self.zctrl.get_value2index()
 		if self.zctrl.labels:
 			#logging.debug("ZCTRL SYNC LABEL => {}".format(self.zctrl.get_value2label()))
 			val=self.zctrl.get_label2value(self.zctrl.get_value2label())