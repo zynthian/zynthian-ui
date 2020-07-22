d8dea94cf5a1eacf6edb6d0699e491fe8565a294
jofemodo <fernando@zynthian.org>
2020-07-22 19:03:12 +0200

    Change call to function to reflect changes in zynautoconnect (#270).

--- zynthian_gui.py
+++ zynthian_gui.py
@@ -1408,7 +1408,7 @@ class zynthian_gui:
 			if zynthian_gui_config.show_cpu_status:
 				# Get CPU Load
 				#self.status_info['cpu_load'] = max(psutil.cpu_percent(None, True))
-				self.status_info['cpu_load'] = zynautoconnect.get_jack_cpu_load()
+				self.status_info['cpu_load'] = zynautoconnect.get_jackd_cpu_load()
 			else:
 				# Get audio peak level
 				self.status_info['peakA'] = lib_jackpeak.getPeak(0)
@@ -1597,6 +1597,7 @@ class zynthian_gui:
 
 	def get_jackd_samplerate(self):
 		return zynautoconnect.get_jackd_samplerate()
+
 
 	def get_jackd_blocksize(self):
 		return zynautoconnect.get_jackd_blocksize()
