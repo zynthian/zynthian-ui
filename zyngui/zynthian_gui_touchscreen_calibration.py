3ab58f15a8b8f70b580439e0aa4aea5cf5a60948
riban <brian@riban.co.uk>
2021-01-03 01:39:41 +0000

    Touchscreen calibration: Improved device detection. Repeat calibration if touches too close together. Change colour of target whilst touched. Don't change calibration until second point pressed. Increase timout to 15s. Stop timeout if finger pressed.

--- zyngui/zynthian_gui_touchscreen_calibration.py
+++ zyngui/zynthian_gui_touchscreen_calibration.py
@@ -26,15 +26,15 @@
 import tkinter
 import logging
 import tkinter.font as tkFont
-from PIL import Image, ImageTk
-from threading import Timer
+from threading import Timer, Thread
 from subprocess import run,PIPE
 from datetime import datetime # Only to timestamp config file updates
+from evdev import InputDevice, ecodes
+from select import select
+import os
 
 # Zynthian specific modules
 from . import zynthian_gui_config
-
-import time
 
 # Little class to represent x,y coordinates
 class point:
@@ -58,7 +58,7 @@ class zynthian_gui_touchscreen_calibration:
 		self.zyngui=zynthian_gui_config.zyngui
 		self.height = zynthian_gui_config.display_height
 		self.width = zynthian_gui_config.display_width
-		self.debounce = 0.1 * self.height
+		self.debounce = 0.5 * self.height # Touches cannot be closer than this
 
 		# Main Frame
 		self.main_frame = tkinter.Frame(zynthian_gui_config.top,
@@ -74,8 +74,6 @@ class zynthian_gui_touchscreen_calibration:
 			bg="black",
 			bd=0,
 			highlightthickness=0)
-		self.canvas.bind('<ButtonRelease-1>', self.onRelease)
-		self.canvas.bind('<Button-1>', self.onPress)
 		
 		# Instruction text
 		self.instruction_text = self.canvas.create_text(self.width / 2,
@@ -94,10 +92,10 @@ class zynthian_gui_touchscreen_calibration:
 			font=(zynthian_gui_config.font_family,zynthian_gui_config.font_size,"normal"),
 			fill="red")
 		self.timer = Timer(interval=1, function=self.onTimer)
-		self.timeout = 7 # Period in seconds after last touch until sceen closes with no change
+		self.timeout = 15 # Period in seconds after last touch until sceen closes with no change
+		self.pressed = False # True if screen pressed
 		
 		# Coordinate transform matrix
-		self.transform_matrix = [1,0,0, 0,1,0, 0,0,1]
 		self.identify_matrix = [1,0,0, 0,1,0, 0,0,1]
 		self.display_points = [point(self.width * 0.15, self.height * 0.15), point(self.width * 0.85, self.height * 0.85)]
 		self.touch_points = [point(), point()]
@@ -108,92 +106,128 @@ class zynthian_gui_touchscreen_calibration:
 		self.crosshair_circle = self.canvas.create_oval(
 			self.display_points[self.index].x - self.crosshair_size * 0.8, self.display_points[self.index].y - self.crosshair_size * 0.8,
 			self.display_points[self.index].x + self.crosshair_size * 0.8, self.display_points[self.index].y + self.crosshair_size * 0.8,
-			width=3, outline="white", tags="crosshairs")
+			width=3, outline="white", tags=("crosshairs","crosshairs_circles"))
 		self.crosshair_inner_circle = self.canvas.create_oval(
 			self.display_points[self.index].x - self.crosshair_size * 0.2, self.display_points[self.index].y - self.crosshair_size * 0.2,
 			self.display_points[self.index].x + self.crosshair_size * 0.2, self.display_points[self.index].y + self.crosshair_size * 0.2,
-			width=3, outline="white", tags="crosshairs")
+			width=3, outline="white", tags=("crosshairs","crosshairs_circles"))
 		self.crosshair_vertical = self.canvas.create_line(
 			self.display_points[self.index].x, self.display_points[self.index].y - self.crosshair_size,
 			self.display_points[self.index].x, self.display_points[self.index].y - self.crosshair_size,
-			width=3, fill="white", tags="crosshairs")
+			width=3, fill="white", tags=("crosshairs","crosshairs_lines"))
 		self.crosshair_horizontal = self.canvas.create_line(
 			self.display_points[self.index].x - self.crosshair_size, self.display_points[self.index].y,
 			self.display_points[self.index].x + self.crosshair_size, self.display_points[self.index].y,
-			width=3, fill="white", tags="crosshairs")
+			width=3, fill="white", tags=("crosshairs","crosshairs_lines"))
 		
 		self.canvas.pack()
 		self.device_name = None # Name of selected device
-		self.devices = {} # Dictionary of device properties indexed by device name
+		self.ctm = [0.0 for i in range(9)] # Coordinate Transformation Matrix
 
+
+	#	Run xinput
+	#	args: List of arguments to pass to xinput
+	#	Returns: Output of xinput as string
+	#	Credit: https://github.com/reinderien/xcalibrate
+	def xinput(self, *args):
+		return run(args=('/usr/bin/xinput', *args),
+			stdout=PIPE, check=True,
+			universal_newlines=True).stdout
+
+
+	#	Tread waiting for first touch to detect touch interface
+	def detectDevice(self):
+		# Populate list of absolute x/y devices
+		devices = []
+		for filename in os.listdir("/dev/input"):
+			if filename.startswith("event"):
+				device = InputDevice("/dev/input/%s" % (filename))
+				if ecodes.EV_ABS in device.capabilities().keys():
+					devices.append(device)
+		# Loop until we get a touch button event or the view hides
+		running = True
+		while running and self.shown:
+			r, w, x = select(devices, [], []) # Wait for any of the devices to trigger an event
+			for device in r: # Iterate through all devices that have triggered events
+				for event in device.read(): # Iterate through all events from each device
+					if event.code == ecodes.BTN_TOUCH: #TODO: Handle other button press - can we use ecode.BTN?
+						self.setDevice(device.name)
+						self.canvas.bind('<Button-1>', self.onPress)
+						self.canvas.bind('<ButtonRelease-1>', self.onRelease)
+						running = False
+
+
+	#	Set the device to configure
+	#	name: Device name
+	def setDevice(self, name):
+		self.device_name = name
+		self.canvas.itemconfig(self.device_text, text=name)
+		#TODO: Do we need to set ctm here?
+		props = self.xinput('--list-props', name)
+		ctm_start = props.find('Coordinate Transformation Matrix')
+		ctm_end = props.find("\n", ctm_start)
+		if ctm_start < 0 or ctm_end < 0:
+			return
+		ctm_start += 40
+		node_start = props.find('Device Node')
+		node_start = props.find('"', node_start)
+		node_end = props.find('"', node_start + 1)
+		if node_start < 0 or node_end < 0:
+			return
+		self.ctm = [float(x) for x in props[ctm_start:ctm_end].split(', ')]
+		
 	
-	#	Handle touch event
+	#	Handle touch press event
 	#	event: Event including x,y coordinates
 	def onPress(self, event):
-		self.countdown = self.timeout # Reset countdown timer when screen touched
-		if not self.device_name:
-			self.index = 0
-			self.detectTouchscreens()
-			for device in self.devices:
-				# Reset all calibration to increase chance of first press triggering and identifying the device
-				self.setCalibration(device, self.identify_matrix)
+		self.canvas.itemconfig("crosshairs_lines", fill="red")
+		self.canvas.itemconfig("crosshairs_circles", outline="red")
+		self.pressed = True
 
-	
+
 	#	Handle touch release event
 	#	event: Event including x,y coordinates
 	def onRelease(self, event):
-		self.countdown = self.timeout # Reset countdown timer when screen touched
+		self.canvas.itemconfig("crosshairs_lines", fill="white")
+		self.canvas.itemconfig("crosshairs_circles", outline="white")
+		self.pressed = False
 		if not self.device_name:
-			for device in self.devices:
-				last_touch = self.getLastTouch(device)
-				if last_touch.x != 0 or last_touch.y != 0:
-					self.device_name = device
-				else:
-					self.setCalibration(device, self.devices[device]["ctm"]) # Reset calibration of other devices that we changed during detection
-			if self.device_name:
-				self.canvas.itemconfig(self.device_text, text=self.device_name)
-			else:
 				return
 		if self.index < 2:
-			if self.index > 0:
-				# Debounce
-				if abs(event.x - self.touch_points[self.index - 1].x) < self.debounce and abs(event.y - self.touch_points[self.index - 1].y) < self.debounce:
-					return
 			# More points to acquire
 			self.touch_points[self.index].x = event.x
 			self.touch_points[self.index].y = event.y
 			self.index += 1
 		if self.index > 1:
-			# Get average coords for corners of touch rectangle
-			min_x = self.touch_points[0].x
-			max_x = self.touch_points[1].x
-			min_y = self.touch_points[0].y
-			max_y = self.touch_points[1].y
-			if min_x == max_x or min_y == max_y:
-				return #TODO: Check if this condition causes issue elsewhere
-			# Check for rotation
-			a = self.width * 0.7 / (max_x - min_x)
-			if min_x < max_x:
-				c = (self.width * 0.15 - a * min_x) / self.width
+			# Debounce
+			if abs(self.touch_points[0].x - self.touch_points[1].x) < self.debounce and abs(self.touch_points[0].y - self.touch_points[1].y) < self.debounce:
+				self.index = 0
 			else:
-				c = (self.width * 0.15 - a * min_x) / self.width
-			e = self.height * 0.7 / (max_y - min_y)
-			if min_y < max_y:
-				f = (self.height * 0.15 - e * min_y) / self.height
-			else:
-				f = (self.height * 0.15 - e * min_y) / self.height
-			self.transform_matrix[0] = a
-			self.transform_matrix[2] = c
-			self.transform_matrix[4] = e
-			self.transform_matrix[5] = f
-			self.setCalibration(self.device_name, self.transform_matrix, True)
-			#TODO: Allow user to check calibration
-			self.hide()
-			return
+				min_x = self.touch_points[0].x
+				max_x = self.touch_points[1].x
+				min_y = self.touch_points[0].y
+				max_y = self.touch_points[1].y
+				if min_x == max_x or min_y == max_y:
+					return #TODO: Check if this condition causes issue elsewhere
+				# Check for rotation
+				a = self.width * 0.7 / (max_x - min_x)
+				if min_x < max_x:
+					c = (self.width * 0.15 - a * min_x) / self.width
+				else:
+					c = (self.width * 0.15 - a * min_x) / self.width
+				e = self.height * 0.7 / (max_y - min_y)
+				if min_y < max_y:
+					f = (self.height * 0.15 - e * min_y) / self.height
+				else:
+					f = (self.height * 0.15 - e * min_y) / self.height
+				self.setCalibration(self.device_name, [a, 0, c, 0, e, f, 0, 0, 1], True)
+				#TODO: Allow user to check calibration
+				self.hide()
+				return
 		self.drawCross()
 
 	
-	#	Draws the crosshairs for touch registration for current index (0..3)
+	#	Draws the crosshairs for touch registration for current index (0..1)
 	def drawCross(self):
 		if self.index > 1:
 			return
@@ -210,79 +244,6 @@ class zynthian_gui_touchscreen_calibration:
 			self.display_points[self.index].x - self.crosshair_size * 0.2, self.display_points[self.index].y - self.crosshair_size * 0.2,
 			self.display_points[self.index].x + self.crosshair_size * 0.2, self.display_points[self.index].y + self.crosshair_size * 0.2)
 		self.canvas.itemconfig("crosshairs", state=tkinter.NORMAL)
-
-
-	#	Get last touch coordinates from device
-	#	Returns point object with last touch coordinates
-	def getLastTouch(self, device):
-		try:
-			result = run(["xinput", "--query-state", device], stdout=PIPE).stdout.decode()
-			a = result.find("valuator[0]")
-			b = result.find("\n", a)
-			x = int(result[a+12:b])
-			a = result.find("valuator[1]")
-			b = result.find("\n", a)
-			y = int(result[a+12:b])
-		except:
-			logging.warning("Failed to detect touchscreen last touch")
-			return point(0,0)
-		return point(x,y)
-
-
-	#	Populate list of touchscreens with relevant properties
-	def detectTouchscreens(self):
-		self.devices = {}
-		result = run(["xinput", "--list", "--name-only"], stdout=PIPE).stdout.decode().split("\n")
-		# Get properties and check for calibration option
-		for device in result:
-			if device == "":
-				continue
-			properties = run(["xinput", "--list", "--long", device], stdout=PIPE).stdout.decode()
-			if properties.find("master pointer") > 0:
-				continue; # Don't want the master device
-			if properties.find("Type: XITouchClass") > 0:
-				config = {}
-				status = run(["xinput", "--query-state", device], stdout=PIPE).stdout.decode()
-				a = properties.find("Abs MT Position X")
-				b = properties.find("Range:", a)
-				c = properties.find("\n",b)
-				d = properties[b+7:c]
-				e = d.split(" - ")
-				min_x = float(e[0])
-				max_x = float(e[1])
-				a = properties.find("Abs MT Position Y")
-				b = properties.find("Range:", a)
-				c = properties.find("\n",b)
-				d = properties[b+7:c]
-				e = d.split(" - ")
-				min_y = float(e[0])
-				max_y = float(e[1])
-				config["min"] = point(min_x,min_y)
-				config["max"] = point(max_x,max_y)
-				config["ctm"] = self.getMatrix(device)
-				self.devices[device] = config
-
-
-	#	Get the current calibration settings for a device
-	#	device: Name or ID of device
-	#	Returns: Coordinate Transformation Matrix or None on error
-	def getMatrix(self, device):
-		result = run(["xinput", "--list-props", device], stdout=PIPE).stdout.decode()
-		a = result.find("Coordinate Transformation Matrix")
-		b = result.find(":", a)
-		c = result.find("\n", b)
-		if not (b > a and c > b):
-			return None
-		str_matrix = result[b+2:c].split(",")
-		if len(str_matrix) != 9:
-			return None
-		matrix = []
-		try:
-			for value in str_matrix:
-				matrix.append(float(value))
-		except:
-			return None
-		return matrix
 
 
 	#	Apply screen calibration
@@ -302,8 +263,8 @@ class zynthian_gui_touchscreen_calibration:
 				matrix[6],
 				matrix[7],
 				matrix[8])
-			proc = run(["xinput", "--set-prop", device, "Coordinate Transformation Matrix",
-				str(matrix[0]), str(matrix[1]), str(matrix[2]), str(matrix[3]), str(matrix[4]), str(matrix[5]), str(matrix[6]), str(matrix[7]), str(matrix[8])])
+			self.xinput("--set-prop", device, "Coordinate Transformation Matrix",
+				str(matrix[0]), str(matrix[1]), str(matrix[2]), str(matrix[3]), str(matrix[4]), str(matrix[5]), str(matrix[6]), str(matrix[7]), str(matrix[8]))
 			if write_file:
 				# Update config file
 				try:
@@ -358,6 +319,8 @@ class zynthian_gui_touchscreen_calibration:
 			self.drawCross()
 			self.main_frame.grid()
 			self.onTimer()
+			self.detect_thread = Thread(target=self.detectDevice, args=(), daemon=True)
+			self.detect_thread.start()
 
 
 	#	Handle one second timer trigger
@@ -366,13 +329,11 @@ class zynthian_gui_touchscreen_calibration:
 			self.canvas.itemconfig(self.countdown_text, text="Closing in %ds" % (self.countdown))
 			if self.countdown <= 0:
 				self.hide()
-				if self.device_name:
-					# Timeout so restore previous config
-					self.setCalibration(self.device_name, self.devices[self.device_name]["ctm"])
-			else:
-				self.timer = Timer(interval=1, function=self.onTimer)
-				self.timer.start()
+				return
+			if not self.pressed:
 				self.countdown -= 1
+			self.timer = Timer(interval=1, function=self.onTimer)
+			self.timer.start()
 
 
 	#	Handle zyncoder read - called by parent when zyncoders updated
