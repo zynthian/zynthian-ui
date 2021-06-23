479efeff5eb113ec83d154f2d6592421234ef7e0
riban <brian@riban.co.uk>
2021-06-23 17:29:01 +0100

    Fixes syntax and modifies graph manager wireframe.

--- zyngine/zynthian_graph_manager.py
+++ zyngine/zynthian_graph_manager.py
@@ -27,8 +27,13 @@
 # The purpose of this module is to manage the audio and MIDI processing modules
 # that are loaded in Zynthian and the routing of interconnects between these
 # modules and to physical interfaces. It manages the graph (nodes and
-# interconnects) and the configuration of each node (synth engine, plugin,
+# interconnects) and the configuration of each engine (synth, audio plugin,
 # etc.). The state of the graph can be saved as a snapshot.
+#
+# Node: Graph node, either JACK port or LV2 control port. May be identified by Engine:port
+# Link: Interconnect between nodes
+# Engine: Module processing audio/MIDI/control signals which contains nodes, e.g. LV2 plugin, synth engine, etc.
+#
 
 import os
 import sys
@@ -50,57 +55,49 @@ class zynthian_graph_manager():
 
 	# Function to initialise instance of class
 	def __init__(self):
-		self.nodes = [] # List of nodes in graph, each derived from zynthian_engine
+		self.engines = [] # List of engines in graph, each derived from zynthian_engine
 		self.links = [] # List of interconnects (src_port, dst_port) Is this required or can be deduced by JACK graph?
 		#	Links describe one or more JACK routes. They may be mono or multichannel audio or MIDI.
-		#	Each node object is derived zynthian_engine which defines its input and output ports, grouping individual i/o as appropriate to allow automatic connection and rule-based routing decisions, e.g. a mono output connecting to a stereo input will send mono to both
+		#	Each engine object is derived zynthian_engine which defines its input and output ports, grouping individual i/o as appropriate to allow automatic connection and rule-based routing decisions, e.g. a mono output connecting to a stereo input will send mono to both
 		# Links can be uniquely identified by the source and destination ports. This can be represented within a session with port UUIDs and within snapshot by list of destination ports each source port is connected to.
 
 
-	# Function to add a node to the graph
-	# node: Object of type zynthian_engine
-	# link: Interconnect to insert new node (Optional - Default: create new chain)
-	def add_node(self, node, link=None):
+	# Function to add an engine to the graph
+	# engine: Engine object to insert
+	# source: Engine object immediately before insert point. (Optional - None to auto connect to physical input)
+	# destination: Engine object immediately after insert point. (Optional - None to auto connect to new mixer input)
+	def add_engine(self, engine, source=None, destination=None):
 		pass
 
 
-	# Function to remove a node from the graph
-	# node: Object to remove
-	# Note: This destroys the node object
-	def remove_node(self, node)
+	# Function to remove an engine from the graph
+	# engine: Object to remove
+	# Note: This destroys the engine object
+	def remove_engine(self, engine):
 		pass
 
 
-	# Function to move a node within the graph
-	# node: Object to move
-	# link: Interconnect to insert node
-	# Returns: True on success otherwise node is not moved
-	# Note: If no more nodes exist in original chain and no input is routed then chain is destroyed
-	def move_node(self, node, link):
+	# Function to move an engine within the graph
+	# engine: Object to move
+	# link: Interconnect to insert engine
+	# Returns: True on success otherwise engine is not moved
+	# Note: If no more engines exist in original chain and no input node is routed then chain is destroyed
+	# Note: All existing connections to engine are removed and default connections are made in new position
+	def move_engine(self, engine, link):
 		pass
 
 
 	# Function to remove chain from graph
 	# chain: Index of chain to remove
-	# Note: This destroys all nodes in the chain
+	# Note: This destroys all engines in the chain
 	def remove_chain(self, index):
 		pass
 
 
 	# Function to remove all chains from graph
-	# Note: This destroys all nodes in all chains
+	# Note: This destroys all engines in all chains
 	def reset(self, index):
 		pass
-	
-
-	# Function to get the id of an interconnect
-	# source_node: Source object of link
-	# source_port: Port on source object that link connects
-	# destination_node: Destination object of link
-	# destination_port: Port on destination object that link connects
-	# Returns: id of link or None if link does not exist
-	def get_interconnect(self, source_node, source_port, destination_node, destination_port):
-		return None
 
 
 	# Function to get list of chains
@@ -108,39 +105,44 @@ class zynthian_graph_manager():
 		return []
 
 
-	# Function to get list of nodes in a chain
+	# Function to get list of engines in a chain
 	# chain: id of chain to query
-	def get_nodes(self, chain):
+	def get_engines(self, chain):
 		return []
 
 
-	# Function to get list of inputs to a node
-	# node: Object whose inputs are being queried
+	# Function to get list of inputs to an engine
+	# engine: Object whose inputs are being queried
 	# Returns: List of inputs
-	def get_node_inputs(self, node):
+	def get_engine_inputs(self, engine):
 		return []
 
 
-	# Function to get list of outputs from a node
-	# node: Object whose outputs are being queried
+	# Function to get list of outputs from an engine
+	# engine: Object whose outputs are being queried
 	# Returns: List of outputs
-	def get_node_outputs(self, node):
+	def get_engine_outputs(self, engine):
 		return []
 
 
-	# Function to get list of interconnects to a node input
-	# node: Object whose input is being queried
-	# input: id of node input
-	def get_input_links(self, node, input):
+	# Function to get list of interconnects to an input node
+	# node: Input node being queried
+	def get_input_links(self, node):
 		return []
 
 
-	# Function to get list of interconnects from a node output
-	# node: Object whose output is being queried
-	# input: id of node output
-	def get_input_links(self, node, output):
+	# Function to get list of interconnects from an output node
+	# node: node whose output is being queried
+	def get_output_links(self, node):
 		return []
 
+
+	# Function to connect an engine's default audio outputs to an engine's default audio inputs
+	# source: Source engine object
+	# destination: Destination engine object
+	def connect(self, source, destination):
+		pass
+	
 
 	# Function to save graph as a snapshot
 	# name: Name of snapshot file
