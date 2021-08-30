zynthian/zynthian-ui

commit fc2b8347e6ea27b7140d8923645012d76b9ec676

Fix directory listing: Avoid failing when a file is found.

zyngine/zynthian_engine.py
@@ -333,17 +333,18 @@ class zynthian_engine(zynthian_basic_engine):
 			dn=dpd[0]
 			try:
 				for f in sorted(os.listdir(dp)):
-					if exclude_empty and next(os.scandir(join(dp,f)), None) is None:
+					dpath = join(dp,f)
+					if not os.path.isdir(dpath) or (exclude_empty and next(os.scandir(dpath), None) is None):
 						continue
-					if not f.startswith('.') and isdir(join(dp,f)):
+					if not f.startswith('.') and isdir(dpath):
 						title,ext=os.path.splitext(f)
 						title=str.replace(title, '_', ' ')
 						if dn!='_': title=dn+'/'+title
-						#print("dirlist => "+title)
-						res.append([join(dp,f),i,title,dn,f])
+						res.append([dpath,i,title,dn,f])
 						i=i+1
-			except:
-				pass
+			except Exception as e:
+				logging.error("Parsing soundfont directory '{}' => {}".format(f,e))
+
 
 		return res
 

commit 895095f38855e8c6143e4125514ca88ed5566fa7

Fix bug introduced in fc2b8347e6ea27b7140d8923645012d76b9ec676.

zyngine/zynthian_engine.py
@@ -318,7 +318,8 @@ class zynthian_engine(zynthian_basic_engine):
 						#print("filelist => "+title)
 						res.append([join(dp,f),i,title,dn,f])
 						i=i+1
-			except:
+			except Exception as e:
+				#logging.warning("Can't access directory '{}' => {}".format(dp,e))
 				pass
 
 		return res
@@ -344,8 +345,8 @@ class zynthian_engine(zynthian_basic_engine):
 						res.append([dpath,i,title,dn,f])
 						i=i+1
 			except Exception as e:
-				logging.error("Parsing soundfont directory '{}' => {}".format(f,e))
-
+				#logging.warning("Can't access directory '{}' => {}".format(dp,e))
+				pass
 
 		return res
 