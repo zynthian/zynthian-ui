f45e6b6023d6cbbd78e5c3cba531b1117898102d
jofemodo <fernando@zynthian.org>
2022-01-17 10:29:09 +0100

    Add "max" ticks to spectrum graph.

--- zyngui/zynthian_gui_spectr30.py
+++ zyngui/zynthian_gui_spectr30.py
@@ -40,12 +40,16 @@ from zyngui import zynthian_gui_control
 
 class zynthian_gui_spectr30(zynthian_gui_control.zynthian_gui_control):
 
+	band_freqs = [25, 31, 40, 50, 63, 80, 100, 125, 160, 200, 250, 315, 400, 500, 630, 800, 
+			   1000, 1250, 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000, 12500, 16000, 20000]
+
 	def __init__(self):
 		super().__init__('1/3 Octave Spectrum')
 
-		self.n_bands = 30
+		self.n_bands = len(self.band_freqs)
 		self.mon_layer = None
 		self.mon_bars = []
+		self.mon_ticks = []
 		self.mon_thread = None
 
 		# Geometry vars
@@ -54,6 +58,7 @@ class zynthian_gui_spectr30(zynthian_gui_control.zynthian_gui_control):
 		else:
 			lbw = self.lb_width + 2
 		self.bar_width = int(lbw/self.n_bands)
+		self.tick_height = int(self.lb_height/80)
 		self.spec_padx = int((lbw%self.n_bands)/2)
 		self.spec_pady=0
 		self.spec_width=self.lb_width-2*self.spec_padx
@@ -72,10 +77,11 @@ class zynthian_gui_spectr30(zynthian_gui_control.zynthian_gui_control):
 		self.listbox.grid_forget()
 		self.spec_canvas.grid(sticky="wens", padx=(self.spec_padx, self.spec_padx), pady=(self.spec_pady, self.spec_pady))
 
-		# Create Bars
+		# Create bars & ticks
 		x0 = 0
 		for i in range(self.n_bands):
-			self.mon_bars.append(self.spec_canvas.create_rectangle(x0, self.spec_height, x0 + self.bar_width, self.spec_height, fill=zynthian_gui_config.color_on))
+			self.mon_bars.append(self.spec_canvas.create_rectangle(x0, self.spec_height, x0 + self.bar_width, self.spec_height, fill=zynthian_gui_config.color_hl))
+			self.mon_ticks.append(self.spec_canvas.create_rectangle(x0, self.spec_height, x0 + self.bar_width, self.spec_height, fill=zynthian_gui_config.color_on))
 			x0 += self.bar_width 
 
 		# Start Thread
@@ -96,23 +102,26 @@ class zynthian_gui_spectr30(zynthian_gui_control.zynthian_gui_control):
 
 
 	def refresh_bars(self):
+		i = 0
 		x0 = 0
-		items = self.zyngui.curlayer.engine.get_lv2_monitors_dict().items()
-		if len(items)>0:
-			i = 0
-			scale = (12.0 + self.zyngui.curlayer.controllers_dict['UIgain'].value) / 12.0
-			for k, v in items:
-				mon_y = int(scale * (100 + v) * self.spec_height / 100)
-				#logging.error("MON {}: {} => {}".format(i,k,mon_y))
-				self.spec_canvas.coords(self.mon_bars[i], x0, self.spec_height, x0 + self.bar_width, self.spec_height - mon_y)
-				x0 += self.bar_width
-				i += 1
-				if i>=self.n_bands:
-					break
-		else:
-			for i in range(self.n_bands):
-				self.spec_canvas.coords(self.mon_bars[i], x0, self.spec_height, x0 + self.bar_width, self.spec_height)
-				x0 += self.bar_width
+		monitors = self.zyngui.curlayer.engine.get_lv2_monitors_dict()
+		scale = (12.0 + self.zyngui.curlayer.controllers_dict['UIgain'].value) / 12.0
+		for freq in self.band_freqs:
+			try:
+				bar_y = int(scale * (100 + monitors["band{}".format(freq)]) * self.spec_height / 100)
+			except:
+				bar_y = 0
+			try:
+				tick_y = int(scale * (100 + monitors["max{}".format(freq)]) * self.spec_height / 100)
+			except:
+				tick_y = 0
+
+			#logging.error("MON {}: {}, {}".format(i,bar_y,tick_y))
+			self.spec_canvas.coords(self.mon_bars[i], x0, self.spec_height, x0 + self.bar_width, self.spec_height - bar_y)
+			self.spec_canvas.coords(self.mon_ticks[i], x0, self.spec_height - tick_y, x0 + self.bar_width, self.spec_height - tick_y - self.tick_height)
+
+			x0 += self.bar_width
+			i += 1
 
 
 #------------------------------------------------------------------------------
