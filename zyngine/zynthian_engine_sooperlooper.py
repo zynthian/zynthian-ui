76361f7182acff35266f5deba8ad2238cee7c33e
riban <brian@riban.co.uk>
2022-07-25 17:27:39 +0100

    Fixes to SooperLooper

--- zyngine/zynthian_engine_sooperlooper.py
+++ zyngine/zynthian_engine_sooperlooper.py
@@ -47,7 +47,7 @@ class zynthian_engine_sooperlooper(zynthian_engine):
 		'wet',					# range 0 -> 1
 	#	'input_gain',			# range 0 -> 1
 		'rate',        			# range 0.25 -> 4.0
-		'scratch_pos',			# range 0 -> 1
+	#	'scratch_pos',			# range 0 -> 1
 	#	'delay_trigger',		# any changes
 		'quantize',				# 0 = off, 1 = cycle, 2 = 8th, 3 = loop
 		'round',				# 0 = off,  not 0 = on
@@ -60,7 +60,6 @@ class zynthian_engine_sooperlooper(zynthian_engine):
   	#	'use_common_ins',		# 0 = off,  not 0 = on
   	#	'use_common_outs',		# 0 = off,  not 0 = on
   		'relative_sync',		# 0 = off, not 0 = on
-		'smart_eighths',		# 0 = off, not 0 = on (undocumented)
   	#	'use_safety_feedback',	# 0 = off, not 0 = on
   	#	'pan_1',				# range 0 -> 1
   	#	'pan_2',				# range 0 -> 1
@@ -83,8 +82,9 @@ class zynthian_engine_sooperlooper(zynthian_engine):
   	#	'rate_output',			# Used to detect direction but must use register_auto_update
 	#	'in_peak_meter',		# absolute float sample value 0.0 -> 1.0 (or higher)
   	#	'out_peak_meter',		# absolute float sample value 0.0 -> 1.0 (or higher)
-  		'is_soloed',			# 1 if soloed, 0 if not
+  	#	'is_soloed',			# 1 if soloed, 0 if not
 		'stretch_ratio',		# 0.5 -> 4.0 (undocumented)
+	#	'tempo_stretch',		# 0 = off, not 0 = on (undocumented)
 		'pitch_shift'			# -12 -> 12 (undocumented)
 	]
 
@@ -95,13 +95,14 @@ class zynthian_engine_sooperlooper(zynthian_engine):
 		'wet',					# range 0 -> 1  affects common output level
 		'input_gain',			# range 0 -> 1  affects common input gain
 		'sync_source',			# -3 = internal,  -2 = midi, -1 = jack, 0 = none, # > 0 = loop number (1 indexed) 
-		#'tap_tempo',			# any changes
+	#	'tap_tempo',			# any changes
 		'save_loop',			# any change triggers quick save, be careful
 		'auto_disable_latency',	# when 1, disables compensation when monitoring main inputs
 		'select_next_loop',		# any changes
 		'select_prev_loop',		# any changes
 		'select_all_loops',		# any changes
 		'selected_loop_num',	# -1 = all, 0->N selects loop instances (first loop is 0, etc) 
+		'smart_eighths',		# 0 = off, not 0 = on (undocumented)
 	]
 
 	SL_STATES ={
@@ -115,9 +116,9 @@ class zynthian_engine_sooperlooper(zynthian_engine):
 		6: {'name': 'Multiplying', 'symbol': 'multiply'},
 		7: {'name': 'Inserting', 'symbol': 'insert'},
 		8: {'name': 'Replacing', 'symbol': 'replace'},
-		9: {'name': 'Delay', 'symbol': 'delay'},
+		9: {'name': 'Delay', 'symbol': None},
 		10: {'name': 'Muted', 'symbol': 'mute'},
-		11: {'name': 'Scratching', 'symbol': 'scratch'},
+		11: {'name': 'Scratching', 'symbol': None},
 		12: {'name': 'OneShot', 'symbol': 'oneshot'},
 		13: {'name': 'Substitute', 'symbol': 'substitute'},
 		14: {'name': 'Paused', 'symbol': 'pause'},
@@ -156,49 +157,42 @@ class zynthian_engine_sooperlooper(zynthian_engine):
 			['replace', 'replace', {'value':0, 'value_max':1, 'labels':['off', 'on'], 'is_toggle':True}],
 			['substitute', 'substitute', {'value':0, 'value_max':1, 'labels':['off', 'on'], 'is_toggle':True}],
 			['insert', 'insert', {'value':0, 'value_max':1, 'labels':['off', 'on'], 'is_toggle':True}],
-			['delay', 'delay', {'value':0, 'value_max':1, 'labels':['off', 'on', 'on']}],
 			['undo/redo', 'undo/redo', {'value':1, 'labels':['<', '<>', '>']}],
 			['trigger', 'trigger;', {'value':0, 'value_max':1, 'labels':['off', 'on'], 'is_toggle':True}],
 			['mute', 'mute', {'value':0, 'value_max':1, 'labels':['off', 'on'], 'is_toggle':True}],
 			['oneshot', 'once', {'value':0, 'value_max':1, 'labels':['off', 'on'], 'is_toggle':True}],
-			['solo', 'solo', {'value':0, 'value_max':1, 'labels':['off', 'on'], 'is_toggle':True}],
 			['pause', 'pause', {'value':0, 'value_max':1, 'labels':['off', 'on'], 'is_toggle':True}],
 			['reverse', 'direction', {'value':1, 'labels':['reverse', 'forward'], 'ticks':[1, 0], 'is_toggle':True}],
-			['scratch', 'scratch', {'value':0, 'value_max':1, 'labels':['normal', 'scratch']}],
-			['rate', 'speed', {'value':1.0, 'value_min':0.25, 'value_max':4.0, 'is_integer':False}],
-			['stretch_ratio', 'stretch', {'value':1.0, 'value_min':0.5, 'value_max':4.0, 'is_integer':False}],
-			['scratch_pos', 'position', {'value':0.0, 'value_max':1.0, 'is_integer':False}], #TODO: Set position max value (dynamic)
-			['pitch_shift', 'pitch', {'value':0.0, 'value_min':-12, 'value_max':12, 'is_integer':False}], #TODO: is pitch integer?
-			['sync_source', 'sync source', {'value':0, 'value_min':-3, 'value_max':1, 'labels':['Internal','MidiClock','Jack/Host','None','Loop1'], 'is_integer':True}], #TODO: Dynamically offer more loops
-			['sync', 'sync operations', {'value':0, 'value_max':1, 'labels':['off', 'on'], 'is_toggle':True}],
-            ['tempo', 'tempo', {'value':120, 'value_min':10, 'value_max':240}],
-            ['eighth_per_cycle', '8th/cycle', {'value':1, 'value_min':1, 'value_max':600}], #TODO: What makes sense for max val?
+			['rate', 'speed', {'value':1.0, 'value_min':0.25, 'value_max':4.0, 'is_integer':False, 'nudge_factor':0.01}],
+			['stretch_ratio', 'stretch', {'value':1.0, 'value_min':0.5, 'value_max':4.0, 'is_integer':False, 'nudge_factor':0.01}],
+			['pitch_shift', 'pitch', {'value':0.0, 'value_min':-12, 'value_max':12, 'is_integer':False, 'nudge_factor':0.1}],
+			['sync_source', 'sync source', {'value':1, 'value_min':-3, 'value_max':1, 'labels':['Internal','MidiClock','Jack/Host','None','Loop1'], 'is_integer':True}], #TODO: Dynamically offer more loops
+			['sync', 'sync operations', {'value':0, 'value_max':1, 'labels':['off', 'on']}],
+            ['eighth_per_cycle', '8th/cycle', {'value':16, 'value_min':1, 'value_max':600}], #TODO: What makes sense for max val?
             ['quantize', 'quantize', {'value':0, 'value_max':3, 'labels':['off', 'cycle', '8th', 'loop']}],
-            ['mute_quantized', 'mute quant', {'value':0, 'value_max':1, 'labels':['off', 'on'], 'is_toggle':True}],
-            ['overdub_quantized', 'overdub quant', {'value':0, 'value_max':1, 'labels':['off', 'on'], 'is_toggle':True}],
-            ['replace_quantized', 'replace quant', {'value':0, 'value_max':1, 'labels':['off', 'on'], 'is_toggle':True}],
-            ['round', 'round', {'value':0, 'value_max':1, 'labels':['off', 'on'], 'is_toggle':True}],
-            ['relative_sync', 'relative sync', {'value':0, 'value_max':1, 'labels':['off', 'on'], 'is_toggle':True}],
-            ['smart_eighths', 'auto 8ths', {'value':0, 'value_max':1, 'labels':['off', 'on'], 'is_toggle':True}],
-            ['playback_sync', 'playback sync', {'value':0, 'value_max':1, 'labels':['off', 'on'], 'is_toggle':True}],
-            ['use_feedback_play', 'play feedback', {'value':0, 'value_max':1, 'labels':['off', 'on'], 'is_toggle':True}],
-            ['tempo_stretch', 'tempo stretch', {'value':0, 'value_max':1, 'labels':['off', 'on'], 'is_toggle':True}],
-			['rec_thresh', 'threshold', {'value':0.0, 'value_max':1.0, 'is_integer':False, 'is_logarithmic': True}],
-			['feedback', 'feedback', {'value':0.5, 'value_max':1.0, 'is_integer':False, 'is_logarithmic': True}],
-			['dry', 'dry', {'value':0.5, 'value_max':1.0, 'is_integer':False, 'is_logarithmic': True}],
-			['wet', 'wet', {'value':0.5, 'value_max':1.0, 'is_integer':False, 'is_logarithmic': True}],
-			['input_gain', 'input gain', {'value':0.5, 'value_max':1.0, 'is_integer':False, 'is_logarithmic': True}]
+            ['mute_quantized', 'mute quant', {'value':0, 'value_max':1, 'labels':['off', 'on'],}],
+            ['overdub_quantized', 'overdub quant', {'value':0, 'value_max':1, 'labels':['off', 'on']}],
+            ['replace_quantized', 'replace quant', {'value':0, 'value_max':1, 'labels':['off', 'on']}],
+            ['round', 'round', {'value':0, 'value_max':1, 'labels':['off', 'on']}],
+            ['relative_sync', 'relative sync', {'value':0, 'value_max':1, 'labels':['off', 'on']}],
+            ['smart_eighths', 'auto 8th', {'value':1, 'value_max':1, 'labels':['off', 'on']}],
+            ['playback_sync', 'playback sync', {'value':0, 'value_max':1, 'labels':['off', 'on']}],
+            ['use_feedback_play', 'play feedback', {'value':0, 'value_max':1, 'labels':['off', 'on']}],
+            ['rec_thresh', 'threshold', {'value':0.0, 'value_max':1.0, 'is_integer':False, 'is_logarithmic': True}],
+			['feedback', 'feedback', {'value':1.0, 'value_max':1.0, 'is_integer':False, 'is_logarithmic': True}],
+			['dry', 'dry', {'value':0.0, 'value_max':1.0, 'is_integer':False, 'is_logarithmic': True}],
+			['wet', 'wet', {'value':1.0, 'value_max':1.0, 'is_integer':False, 'is_logarithmic': True}],
+			['input_gain', 'input gain', {'value':1.0, 'value_max':1.0, 'is_integer':False, 'is_logarithmic': True}]
 		]
 
 		# Controller Screens
 		self._ctrl_screens = [
 			['Rec 1',['record','overdub','multiply','undo/redo']],
-			['Rec 2',['replace','substitute','insert','delay']],
+			['Rec 2',['replace','substitute','insert','undo/redo']],
 			['Play 1',['trigger','oneshot','mute','pause']],
 			['Play 2',['reverse','pitch_shift','rate','stretch_ratio']],
-			['Sync 1',['sync_source','sync','eighth_per_cycle',None]],
-			['Sync 2',['round','relative_sync','smart_eighths', None]],
-			['Sync 3',['play sync','use_feedback_play','tempo_stretch',None]],
+			['Sync 1',['sync_source','sync','eighth_per_cycle','playback_sync']],
+			['Sync 2',['round','relative_sync','smart_eighths', 'use_feedback_play']],
 			['Quantize',['quantize','mute_quantized','overdub_quantized','replace_quantized']],
 			['Levels 1',['rec_thresh', 'feedback', 'dry', 'wet']],
 			['Levels 2',['input_gain', None, None, None]]
@@ -213,9 +207,9 @@ class zynthian_engine_sooperlooper(zynthian_engine):
 	# ---------------------------------------------------------------------------
 
 	def start(self):
-		logging.warning("Starting SooperLooper")
+		#logging.warning("Starting SooperLooper")
 		self.osc_init(self.SL_PORT)
-		self.proc_start_sleep = 1 #TODO: Validate this delay is still required
+		self.proc_start_sleep = 1 #TODO: Cludgy wait - maybe should perform periodic check for server until reachable
 		super().start()
 		liblo.send(self.osc_target, '/sl/0/register_auto_update', ('s', 'state'), ('i', 100), ('s', self.osc_server_url), ('s', '/sl/state'))
 		liblo.send(self.osc_target, '/sl/0/register_auto_update', ('s', 'rate_output'), ('i', 100), ('s', self.osc_server_url), ('s', '/sl/control'))
@@ -263,10 +257,9 @@ class zynthian_engine_sooperlooper(zynthian_engine):
 
 	def send_controller_value(self, zctrl):
 		#logging.warning("{} {}".format(zctrl.symbol, zctrl.value))
-		if zctrl.symbol == 'oneshot' and zctrl.value == 0:
+		if zctrl.symbol in ['oneshot', 'once', 'trigger'] and zctrl.value == 0:
+			# Ignore off signals
 			return
-		elif zctrl.symbol == 'delay':
-			liblo.send(self.osc_target, '/sl/-3/set', ('s', 'delay_trigger'), ('f', zctrl.value))
 		elif zctrl.is_toggle:
 			liblo.send(self.osc_target, '/sl/-3/hit', ('s', zctrl.symbol))
 			if zctrl.symbol == 'trigger':
@@ -293,7 +286,7 @@ class zynthian_engine_sooperlooper(zynthian_engine):
 
 	# Update each mutually exclusive 'state' controller to match current state
 	def update_state(self):
-		logging.warning("State: {}".format(self.state))
+		#logging.warning("State: {}".format(self.state))
 		for state in self.SL_STATES:
 			if self.SL_STATES[state]['symbol']:
 				if state == self.state:
@@ -303,7 +296,7 @@ class zynthian_engine_sooperlooper(zynthian_engine):
 
 
 	def cb_osc_all(self, path, args, types, src):
-		logging.warning("Rx OSC: {} {}".format(path,args))
+		#logging.warning("Rx OSC: {} {}".format(path,args))
 		if path == '/sl/state':
 			# args: i:Loop index, s:control, f:value
 			self.state = int(args[2])
@@ -314,17 +307,11 @@ class zynthian_engine_sooperlooper(zynthian_engine):
 
 		elif path == '/sl/control':
 			if args[1] == 'rate_output':
-				logging.warning('rate: %f', args[2])
 				if args[2] < 0.0:
 					self.zctrls['reverse'].set_value(1, False)
 				else:
 					self.zctrls['reverse'].set_value(0, False)
-			elif args[1] in self.SL_LOOP_PARAMS:
-				try:
-					self.zctrls[args[1]].set_value(args[2], False)
-				except Exception as e:
-					logging.warning("Unsupported tally %s (%f)", args[1], args[2])
-			elif args[1] in self.SL_GLOBAL_PARAMS:
+			elif args[1] in self.SL_LOOP_PARAMS or args[1] in self.SL_GLOBAL_PARAMS:
 				if args[1] == 'selected_loop_num':
 					self.select_loop(args[2])
 				try:
