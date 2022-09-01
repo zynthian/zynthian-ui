0d299f260e7b97d69361c5ed420d204b9b005ea6
jofemodo <fernando@zynthian.org>
2022-09-01 20:05:19 +0200

    Moving up "Clean All" option to a better place.

--- zyngui/zynthian_gui_main.py
+++ zyngui/zynthian_gui_main.py
@@ -49,6 +49,7 @@ class zynthian_gui_main(zynthian_gui_selector):
 		self.list_data.append((self.new_generator_layer,0,"New Generator Chain"))
 		self.list_data.append((self.new_special_layer,0,"New Special Chain"))
 		self.list_data.append((self.snapshots,0,"Snapshots"))
+		self.list_data.append((self.clean_all,0,"Clean All"))
 
 		# Add list of Apps
 		self.list_data.append((None,0,"  APPS"))
@@ -57,7 +58,6 @@ class zynthian_gui_main(zynthian_gui_selector):
 
 		# Add list of System / configuration views
 		self.list_data.append((None,0,"  SYSTEM"))
-		self.list_data.append((self.clean_all,0,"Clean All"))
 		self.list_data.append((self.alsa_mixer,0,"Audio Levels"))
 		self.list_data.append((self.admin,0,"Admin"))
 		self.list_data.append((self.all_notes_off,0,"PANIC! All Notes Off"))
