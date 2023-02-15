2b1a92d8a10bd724c2bfbcd027ba8127ac785fc0
jofemodo <fernando@zynthian.org>
2023-02-15 12:59:04 +0100

    Add LEDs layout for V5 (not finished!!).

--- zyngui/zynthian_wsleds_v5.py
+++ zyngui/zynthian_wsleds_v5.py
@@ -3,7 +3,7 @@
 #******************************************************************************
 # ZYNTHIAN PROJECT: Zynthian GUI
 #
-# Zynthian WSLeds Class for Z2
+# Zynthian WSLeds Class for V5
 #
 # Copyright (C) 2015-2023 Fernando Moyano <jofemodo@zynthian.org>
 #
@@ -35,18 +35,11 @@ class zynthian_wsleds_z2(zynthian_wsleds_base):
 	
 	def __init__(self, zyngui):
 		super().__init__(zyngui)
-		if zynthian_gui_config.wiring_layout == "Z2_V1":
-			# LEDS with PWM1 (pin 13, channel 1)
-			self.dma = 10
-			self.pin = 13
-			self.chan = 1
-			self.num_leds = 25
-		elif zynthian_gui_config.wiring_layout in ("Z2_V2", "Z2_V3"):
-			# LEDS with SPI0 (pin 10, channel 0)
-			self.dma = 10
-			self.pin = 10
-			self.chan = 0
-			self.num_leds = 25
+		# LEDS with SPI0 (pin 10, channel 0)
+		self.dma = 10
+		self.pin = 10
+		self.chan = 0
+		self.num_leds = 20
 
 
 	def update_wsleds(self):
@@ -61,59 +54,21 @@ class zynthian_wsleds_z2(zynthian_wsleds_base):
 		else:
 			self.wsleds.setPixelColor(0, self.wscolor_light)
 
-		# Active Layer
-		# => Light non-empty layers
-		n = self.zyngui.screens['layer'].get_num_root_layers()
-		main_fxchain = self.zyngui.screens['layer'].get_main_fxchain_root_layer()
-		if main_fxchain:
-			n -= 1
-		for i in range(6):
-			if i < n:
-				self.wsleds.setPixelColor(1 + i, self.wscolor_light)
-			else:
-				self.wsleds.setPixelColor(1 + i, self.wscolor_off)
-		# => Light FX layer if not empty
-		if main_fxchain:
-			self.wsleds.setPixelColor(7, self.wscolor_light)
+		# Audio Mixer
+		if curscreen == "audio_mixer":
+			self.wsleds.setPixelColor(1, self.wscolor_active)
+		elif curscreen == "snapshot":
+			self.wsleds.setPixelColor(1, self.wscolor_admin)
 		else:
-			self.wsleds.setPixelColor(7, self.wscolor_off)
-		# => Light active layer
-		i = self.zyngui.screens['layer'].get_root_layer_index()
-		if i is not None:
-			if main_fxchain and i == n:
-				if curscreen == "control":
-					self.wsleds.setPixelColor(7, self.wscolor_active)
-				else:
-					self.blink(7, self.wscolor_active)
-			elif i < 6:
-				if curscreen == "control":
-					self.wsleds.setPixelColor(1 + i, self.wscolor_active)
-				else:
-					self.blink(1 + i, self.wscolor_active)
+			self.wsleds.setPixelColor(1, self.wscolor_light)
 
 		# Stepseq screen:
 		if curscreen == "zynpad":
-			self.wsleds.setPixelColor(8, self.wscolor_active)
+			self.wsleds.setPixelColor(2, self.wscolor_active)
+		elif curscreen == "pattern_editor":
+			self.wsleds.setPixelColor(2, self.wscolor_admin)
 		else:
-			self.wsleds.setPixelColor(8, self.wscolor_light)
-
-		# Pattern Editor screen:
-		if curscreen == "pattern_editor":
-			self.wsleds.setPixelColor(9, self.wscolor_active)
-		else:
-			self.wsleds.setPixelColor(9, self.wscolor_light)
-
-		# MIDI Recorder screen:
-		if curscreen == "midi_recorder":
-			self.wsleds.setPixelColor(10, self.wscolor_active)
-		else:
-			self.wsleds.setPixelColor(10, self.wscolor_light)
-
-		# Snapshot screen:
-		if curscreen == "snapshot":
-			self.wsleds.setPixelColor(11, self.wscolor_active)
-		else:
-			self.wsleds.setPixelColor(11, self.wscolor_light)
+			self.wsleds.setPixelColor(2, self.wscolor_light)
 
 		# Presets screen:
 		if curscreen in ("preset", "bank"):
@@ -128,6 +83,12 @@ class zynthian_wsleds_z2(zynthian_wsleds_base):
 			self.wsleds.setPixelColor(13, self.wscolor_active)
 		else:
 			self.wsleds.setPixelColor(13, self.wscolor_light)
+
+		# MIDI Recorder screen:
+		if curscreen == "midi_recorder":
+			self.wsleds.setPixelColor(10, self.wscolor_active)
+		else:
+			self.wsleds.setPixelColor(10, self.wscolor_light)
 
 		# REC/PLAY Audio buttons:
 		if 'audio_recorder' in self.zyngui.status_info:
@@ -177,14 +138,6 @@ class zynthian_wsleds_z2(zynthian_wsleds_base):
 		# Left, Bottom, Right button
 		for i in range(3):
 			self.wsleds.setPixelColor(21 + i, self.wscolor_yellow)
-
-		# Audio Mixer/Levels screen
-		if curscreen == "audio_mixer":
-			self.wsleds.setPixelColor(24, self.wscolor_active)
-		elif curscreen == "alsa_mixer":
-			self.wsleds.setPixelColor(24, self.wscolor_admin)
-		else:
-			self.wsleds.setPixelColor(24, self.wscolor_light)
 
 		try:
 			self.screens[curscreen].update_wsleds()
