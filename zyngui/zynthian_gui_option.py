7159ccdb98b91adbab0505961514640941e2273f
jofemodo <fernando@zynthian.org>
2023-06-12 20:59:03 +0200

    + Implement not-closing options dialogs. + Implement options refresh CB, so options list can be refreshed after select.

--- zyngui/zynthian_gui_option.py
+++ zyngui/zynthian_gui_option.py
@@ -38,20 +38,32 @@ class zynthian_gui_option(zynthian_gui_selector):
 	def __init__(self):
 		self.title = ""
 		self.options = {}
+		self.options_cb = None
 		self.cb_select = None
+		self.close_on_select = True
 		super().__init__("Option", True)
 
 
-	def config(self, title, options, cb_select):
+	def config(self, title, options, cb_select, close_on_select=True):
 		self.title = title
-		self.options = options
+		if callable(options):
+			self.options_cb = options
+			self.options = None
+		else:
+			self.options_cb = None
+			self.options = options
 		self.cb_select = cb_select
+		self.close_on_select = close_on_select
+		self.index = 0
 
 
-	def config_file_list(self, title, dpaths, fext, cb_select):
+	def config_file_list(self, title, dpaths, fext, cb_select, close_on_select=True):
 		self.title = title
-		self.cb_select = cb_select
 		self.options = {}
+		self.options_cb = None
+		self.cb_select = cb_select
+		self.close_on_select = close_on_select
+		self.index = 0
 
 		if isinstance(dpaths, str):
 			dpaths = [dpaths]
@@ -77,6 +89,8 @@ class zynthian_gui_option(zynthian_gui_selector):
 	def fill_list(self):
 		i = 0
 		self.list_data = []
+		if self.options_cb:
+			self.options = self.options_cb()
 		for k, v in self.options.items():
 			self.list_data.append((v, i, k))
 			i += 1
@@ -84,9 +98,12 @@ class zynthian_gui_option(zynthian_gui_selector):
 
 
 	def select_action(self, i, t='S'):
-		self.zyngui.close_screen()
+		if self.close_on_select:
+			self.zyngui.close_screen()
 		if self.cb_select and i < len(self.list_data):
 			self.cb_select(self.list_data[i][2], self.list_data[i][0])
+			if not self.close_on_select:
+				self.fill_list()
 
 
 	def set_select_path(self):
