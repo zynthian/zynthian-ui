a24fabead0be089cf02ea442205de12d4a614c52
jofemodo <fernando@zynthian.org>
2023-12-04 15:21:19 +0100

    + Move zynthian_audio_recorder from zyngui to zyngine. + Fix filename generation when no snapshot is loaded.

--- zyngine/zynthian_audio_recorder.py
+++ zyngine/zynthian_audio_recorder.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-#******************************************************************************
+# ******************************************************************************
 # ZYNTHIAN PROJECT: Zynthian Core
 # 
 # Zynthian Audio Recorder Class
@@ -8,7 +8,7 @@
 # Copyright (C) 2015-2022 Fernando Moyano <jofemodo@zynthian.org>
 #                         Brian Walton <riban@zynthian.org>
 #
-#******************************************************************************
+# ******************************************************************************
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -22,7 +22,7 @@
 #
 # For a full copy of the GNU General Public License see the LICENSE.txt file.
 # 
-#******************************************************************************
+# ******************************************************************************
 
 import os
 import logging
@@ -32,28 +32,26 @@ from datetime import datetime
 # Zynthian specific modules
 from zyngui import zynthian_gui_config
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Zynthian Audio Recorder Class
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
-class zynthian_audio_recorder():
 
+class zynthian_audio_recorder:
 
 	def __init__(self, state_manager):
 		self.capture_dir_sdc = os.environ.get('ZYNTHIAN_MY_DATA_DIR', "/zynthian/zynthian-my-data") + "/capture"
 		self.ex_data_dir = os.environ.get('ZYNTHIAN_EX_DATA_DIR', "/media/root")
 		self.rec_proc = None
-		self.armed = set() # List of chains armed to record
+		self.armed = set()  # List of chains armed to record
 		self.state_manager = state_manager
 		self.filename = None
 
-
 	def get_status(self):
-		#TODO: This could provide different status now that playback has been removed
+		# TODO: This could provide different status now that playback has been removed
 		if self.rec_proc:
 			return "REC"
 		return None
-
 
 	def get_new_filename(self):
 		exdirs = zynthian_gui_config.get_external_storage_dirs(self.ex_data_dir)
@@ -61,11 +59,11 @@ class zynthian_audio_recorder():
 			path = exdirs[0]
 		else:
 			path = self.capture_dir_sdc
-		try:
+		if self.state_manager.last_snapshot_fpath and len(self.state_manager.last_snapshot_fpath) > 4:
 			filename = os.path.basename(self.state_manager.last_snapshot_fpath[:-4])
-		except:
+		else:
 			filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-		filename = filename.replace("/",";").replace(">",";").replace(" ; ",";")
+		filename = filename.replace("/", ";").replace(">", ";").replace(" ; ", ";")
 		# Append index to file to make unique
 		index = 1
 		files = os.listdir(path)
@@ -73,10 +71,8 @@ class zynthian_audio_recorder():
 			index += 1
 		return "{}/{}.{:03d}.wav".format(path, filename, index)
 
-
 	def arm(self, channel):
 		self.armed.add(channel)
-
 
 	def unarm(self, channel):
 		try:
@@ -84,17 +80,14 @@ class zynthian_audio_recorder():
 		except:
 			logging.info("Channel %d not armed", channel)
 
-
 	def toggle_arm(self, channel):
 		if self.is_armed(channel):
 			self.unarm(channel)
 		else:
 			self.arm(channel)
 
-
 	def is_armed(self, channel):
 		return channel in self.armed
-
 
 	def start_recording(self, processor=None):
 		if self.rec_proc:
@@ -117,19 +110,18 @@ class zynthian_audio_recorder():
 		self.filename = self.get_new_filename()
 		cmd.append(self.filename)
 
-		logging.info("STARTING NEW AUDIO RECORD ...")
+		logging.info(f"STARTING NEW AUDIO RECORD '{self.filename}'...")
 		try:
 			self.rec_proc = Popen(cmd)
 		except Exception as e:
 			logging.error("ERROR STARTING AUDIO RECORD: %s" % e)
-			self.proc = None
+			self.rec_proc = None
 			return False
 
 		self.state_manager.status_audio_recorder = True
 		if processor:
 			processor.controllers_dict['record'].set_value("recording", False)
 		return True
-
 
 	def stop_recording(self, player=None):
 		if self.rec_proc:
@@ -150,7 +142,6 @@ class zynthian_audio_recorder():
 
 		return False
 
-
 	def toggle_recording(self, player=None):
 		logging.info("TOGGLING AUDIO RECORDING ...")
 		if self.get_status():
@@ -158,7 +149,6 @@ class zynthian_audio_recorder():
 			return False
 		else:
 			self.start_recording(player)
-			return  True
+			return True
 
-
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
