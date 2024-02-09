fcba3cc3bf7db23ef90f613b1d779f6ac1136b79
jofemodo <fernando@zynthian.org>
2024-02-09 11:25:09 +0100

    Use python env "shebang line" for zynthian-ui main programs.

--- zyngine/zynthian_lv2.py
+++ zyngine/zynthian_lv2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # ******************************************************************************
 # ZYNTHIAN PROJECT: Zynthian LV2-plugin management
