7e5793f987e73f9cc21c75ab833002257650e70d
jofemodo <fernando@zynthian.org>
2024-09-04 18:59:38 +0200

    + Fixes to inet radio engine & GUI + Extend default list of radio stations & categories.

--- zyngine/zynthian_engine_inet_radio.py
+++ zyngine/zynthian_engine_inet_radio.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-#******************************************************************************
+# ******************************************************************************
 # ZYNTHIAN PROJECT: Zynthian Engine (zynthian_engine_inetradio)
 #
 # zynthian_engine implementation for internet radio streamer
 #
 # Copyright (C) 2022-2024 Brian Walton <riban@zynthian.org>
 #
-#******************************************************************************
+# ******************************************************************************
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -20,7 +20,7 @@
 #
 # For a full copy of the GNU General Public License see the LICENSE.txt file.
 #
-#******************************************************************************
+# ******************************************************************************
 
 from collections import OrderedDict
 import logging
@@ -33,9 +33,11 @@ from time import sleep
 from . import zynthian_engine
 import zynautoconnect
 
-#------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
 # Internet Radio Engine Class
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
 
 class zynthian_engine_inet_radio(zynthian_engine):
 
@@ -69,19 +71,18 @@ class zynthian_engine_inet_radio(zynthian_engine):
 		self.handle = 0
 		
 		# MIDI Controllers
-		self._ctrls=[
-			['volume',None,50,100],
-			['stream',None,'streaming',['stopped','streaming']],
-			['wait for stream',None, 5, 15]
+		self._ctrls = [
+			['volume', None, 50, 100],
+			['stream', None, 'streaming', ['stopped', 'streaming']],
+			['wait for stream', None, 5, 15]
 		]
 
 		# Controller Screens
-		self._ctrl_screens=[
-			['main',['volume','stream','wait for stream']]
+		self._ctrl_screens = [
+			['main', ['volume', 'stream', 'wait for stream']]
 		]
 
 		self.reset()
-
 
 	# ---------------------------------------------------------------------------
 	# Subproccess Management & IPC
@@ -141,8 +142,6 @@ class zynthian_engine_inet_radio(zynthian_engine):
 			ctrl_dict = processor.controllers_dict
 			ctrl_dict['stream'].set_value('stopped', False)
 
-
-
 	def update_info(self, output):
 		info = re.search("StreamTitle='(.+?)';", output)
 		if info:
@@ -157,9 +156,9 @@ class zynthian_engine_inet_radio(zynthian_engine):
 		if info:
 			self.monitors_dict['bitrate'] = info.group(1)
 
-
 	def start(self):
 		self.handle += 1
+		self.monitors_dict['title'] = self.processors[0].preset_name
 		self.monitors_dict['info'] = "waiting for stream..."
 		self.mon_thread = Thread(target=self.mon_thread_task, args=[self.handle])
 		self.mon_thread.name = "internet radio {}".format(self.handle)
@@ -169,26 +168,23 @@ class zynthian_engine_inet_radio(zynthian_engine):
 		self.monitors_dict['bitrate'] = ""
 		self.mon_thread.start()
 		
-
 	def stop(self):
 		self.handle += 1
 		self.mon_thread.join()
 
+		self.monitors_dict['title'] = ""
 		self.monitors_dict['info'] = ""
 		self.monitors_dict['codec'] = ""
 		self.monitors_dict['audio'] = ""
 		self.monitors_dict['bitrate'] = ""
 
-
 	# ---------------------------------------------------------------------------
 	# Processor Management
 	# ---------------------------------------------------------------------------
 
-
 	# ---------------------------------------------------------------------------
 	# MIDI Channel Management
 	# ---------------------------------------------------------------------------
-
 
 	# ---------------------------------------------------------------------------
 	# Bank Management
@@ -201,18 +197,97 @@ class zynthian_engine_inet_radio(zynthian_engine):
 		except:
 			# Preset file missing or corrupt
 			self.presets = {
-				"Music": [
+				"Ambient": [
+					["http://relax.stream.publicradio.org/relax.mp3", 0, "Relax", "auto", ""],
+					["https://peacefulpiano.stream.publicradio.org/peacefulpiano.aac", 0, "Peaceful Piano", "aac", ""],
+					["http://mp3stream4.abradio.cz/chillout128.mp3", 0, "Radio Chillout - ABradio", "auto", ""],
+					["http://afera.com.pl/afera128.pls", 0, "Radio Afera", "auto", ""],
+					["http://192.111.140.6:8021/listen.pls", 0, "Childside Radio", "auto", ""],
+					["http://usa14.fastcast4u.com/proxy/chillmode", 0, "Chillmode Radio", "auto", ""],
+					#["https://radio.streemlion.com:3590/stream", 0, "Nordic Lodge Copenhagen", "auto", ""]
+				],
+				"Classical": [
+					["http://66.42.114.24:8000/live", 0, "Classical Oasis", "auto", ""],
+					["https://chambermusic.stream.publicradio.org/chambermusic.aac", 0, "Chamber Music", "aac", ""],
+					#["https://live.amperwave.net/playlist/mzmedia-cfmzfmmp3-ibc2.m3u", 0, "The New Classical FM", "auto", ""],
+					#["https://audio-mp3.ibiblio.org/wdav-112k", 0, "WDAV Classical: Mozart Café", "auto", ""],
+					#["https://cast1.torontocast.com:2085/stream", 0, "KISS Classical", "auto", ""]
+				],
+				"Techno, Trance, House, D&B": [
+					#["https://fr1-play.adtonos.com/8105/psystation-minimal", 0, "PsyStation - Minimal Techno", "auto", ""],
+					#["https://strw3.openstream.co/940", 0, "Minimal & Techno on MixLive.ie", "auto", ""],
+					["http://stream.radiosputnik.nl:8002/", 0, "Radio Sputnik", "auto", ""],
+					["http://streaming05.liveboxstream.uk:8047/", 0, "Select Radio", "auto", ""],
+					["http://listener3.mp3.tb-group.fm/clt.mp3", 0, "ClubTime.FM", "auto", ""],
+					["http://stream3.jungletrain.net:8000 /;", 0, "jungletrain.net - 24/7 D&B&J", "auto", ""]
+				],
+				"Hiphop, R&B, Trap": [
+					#["https://hiphop24.stream.laut.fm/hiphop24", 0, "HipHop24", "auto", ""],
+					["http://streams.90s90s.de/hiphop/mp3-192/", 0, "90s90s HipHop", "auto", ""],
+					["https://streams.80s80s.de/hiphop/mp3-192/", 0, "80s80s HipHop", "auto", ""],
+					["http://stream.jam.fm/jamfm-bl/mp3-192/", 0, "JAM FM Black Label", "auto", ""],
+					["http://channels.fluxfm.de/boom-fm-classics/stream.mp3", 0, "HipHop Classics", "auto", ""],
+					#["https: // finesthiphopradio.stream.laut.fm / finesthiphopradio", 0, "Finest HipHop Radio", "auto", ""],
+					#["https://stream.bigfm.de/oldschoolrap/mp3-128/", 0, "bigFM OLDSCHOOL RAP & HIP-HOP", "auto", ""]
+				],
+				"Funk & Soul": [
+					#["https://funk.stream.laut.fm/funk", 0, "The roots of Funk", "auto", ""],
+					#["http://radio.pro-fhi.net:2199/rqwrejez.pls", 0, "Funk Power Radio", "auto", ""],
+					["http://listento.thefunkstation.com:8000", 0, "The Funk Station", "auto", ""],
+					["https://scdn.nrjaudio.fm/adwz1/fr/30607/mp3_128.mp3", 0, "Nostalgie Funk", "auto", ""],
+					["http://funkyradio.streamingmedia.it/play.mp3", 0, "Funky Radio", "auto", ""],
+					["http://listen.shoutcast.com/a-afunk", 0, "Anthology Funk", "auto", ""]
+				],
+				"Reggae, Afrobeat, World music": [
+					["http://ais.rastamusic.com/rastamusic.mp3", 0, "Rastamusic Reggae Radio ", "auto", ""],
+					["https://ais-sa2.cdnstream1.com/2294_128.mp3", 0, "Big Reggae Mix", "auto", ""],
+					["http://hd.lagrosseradio.info/lagrosseradio-reggae-192.mp3", 0, "La Grosse Radio Reggae", "auto", ""],
+					["http://api.somafm.com/reggae.pls", 0, "SomaFM: Heavyweight Reggae", "auto", ""],
+					["http://stream.zenolive.com/n164uxfk8neuv", 0, "UbuntuFM Reggae Radio", "auto", ""],
+					["http://152.228.170.37:8000", 0, "AfroBeats FM", "auto", ""],
+					["https://wdr-cosmo-afrobeat.icecastssl.wdr.de/wdr/cosmo/afrobeat/mp3/128/stream.mp3", 0, "WDR Cosmo - Afrobeat", "auto", ""],
+					["http://stream.zenolive.com/erfqvd71nd5tv", 0, "Rainbow Radio", "auto", ""],
+					["http://usa6.fastcast4u.com:5374/", 0, "Rainbow Radio - UK", "auto", ""],
+					["http://topjam.ddns.net:8100/", 0, "TOP JAM Radio Reggae Dancehall", "auto", ""],
+					["http://stream.jam.fm/jamfm_afrobeats/mp3-192/", 0, "JAM FM Afrobeats", "auto", ""]
+				],
+				"Jazz & Blues": [
+					["http://jazzblues.ice.infomaniak.ch/jazzblues-high.mp3", 0, "Jazz Blues", "auto", ""],
+					["http://live.amperwave.net/direct/ppm-jazz24mp3-ibc1", 0, "Jazz24 - KNKX-HD2", "auto", ""],
+					["http://stream.sublime.nl/web24_mp3", 0, "Sublime Classics", "auto", ""],
+					["http://jazz-wr01.ice.infomaniak.ch/jazz-wr01-128.mp3", 0, "JAZZ RADIO CLASSIC JAZZ", "auto", ""],
+					["http://jzr-piano.ice.infomaniak.ch/jzr-piano.mp3", 0, "JAZZ RADIO PIANO JAZZ", "auto", ""],
+					["http://stream.radio.co/s7c1ea5960/listen", 0, "Capital Jazz Radio", "auto", ""],
+					["http://radio.wanderingsheep.tv:8000/jazzcafe", 0, "Jazz Cafe", "auto", ""],
+					["https://jazz.stream.laut.fm/jazz", 0, "Ministry of Soul", "auto", ""],
+					["https://stream.spreeradio.de/deluxe/mp3-192/", 0, "105‘5 Spreeradio Deluxe", "auto", ""]
+				],
+				"Latin & Afrocuban": [
+					["https://ny.mysonicserver.com/9918/stream", 0 , "La esquina del guaguanco", "auto", ""],
+					["http://tropicalisima.net:8020", 0, "Tropicalisima FM Salsa", "auto", ""],
+					["https://salsa.stream.laut.fm/salsa", 0, "Salsa", "auto", ""],
+					["http://95.216.22.117:8456/stream", 0, "Hola NY Salsa", "auto", ""],
+					["http://stream.zeno.fm/r82w6dp09vzuv", 0, "Salseros", "auto", ""],
+					["http://stream.zenolive.com/tgzmw19rqrquv", 0, "Salsa.fm", "auto", ""],
+					["http://stream.zenolive.com/u27pdewuq74tv", 0, "Salsa Gorda Radio", "auto", ""],
+					#["https://salsa-high.rautemusik.fm/", 0, "RauteMusik SALSA", "auto", ""],
+					#["https://centova.streamingcastrd.net/proxy/bastosalsa/stream", 0, "Basto Salsa Radio", "auto", ""],
+					#["https://usa15.fastcast4u.com/proxy/erenteri", 0, "Radio Salsa Online", "auto", ""],
+					#["https://cloudstream2036.conectarhosting.com:8242", 0, "La Makina del Sabor", "auto", ""],
+					#["https://cloudstream2032.conectarhosting.com/8122/stream", 0, "Salsa Magistral", "auto", ""],
+					#["https://cloud8.vsgtech.co/8034/stream", 0, "Viva la salsa", "auto", ""],
+					#["https://cast1.my-control-panel.com/proxy/salsason/stream", 0, "Salsa con Timba", "auto", ""],
+				],
+				"Pop & Rock": [
+					["http://icy.unitedradio.it/VirginRock70.mp3", 0, "Virgin Rock 70's", "auto", ""],
+					["http://sc3.radiocaroline.net:8030/listen.m3u", 0, "Radio Caroline", "auto", ""]
+				],
+				"Miscellaneous": [
 					["http://stream.radiotime.com/listen.m3u?streamId=10555650", 0, "FIP", "auto", ""],
 					["http://icecast.radiofrance.fr/fipgroove-hifi.aac", 0, "FIP Groove", "aac", ""],
 					["http://direct.fipradio.fr/live/fip-webradio4.mp3", 0, "FIP Radio 4", "auto", ""],
-					["http://jazzblues.ice.infomaniak.ch/jazzblues-high.mp3", 0, "Jazz Blues", "auto", ""],
-					["http://relax.stream.publicradio.org/relax.mp3", 0, "Relax", "auto", ""],
-					["http://icy.unitedradio.it/VirginRock70.mp3", 0, "Virgin Rock 70's", "auto", ""],
-					["https://peacefulpiano.stream.publicradio.org/peacefulpiano.aac", 0, "Peaceful Piano", "aac", ""],
-					["https://chambermusic.stream.publicradio.org/chambermusic.aac", 0, "Chamber Music", "aac", ""],
-					["http://sc3.radiocaroline.net:8030/listen.m3u", 0, "Radio Caroline", "auto", ""]
 				],
-				"Speech": [
+				"Culture & News (speech)": [
 					["http://direct.franceculture.fr/ts/franceculture-midfi.mp3", 0, "France Culture", "auto", ""],
 					["http://wsdownload.bbc.co.uk/worldservice/meta/live/shoutcast/mp3/eieuk.pls", 0, "BBC Radio World Service (English)", "auto", ""]
 				]
@@ -232,8 +307,7 @@ class zynthian_engine_inet_radio(zynthian_engine):
 			presets.append(preset)
 		return presets
 
-
-	def set_preset(self, processor, preset, preload=False):		
+	def set_preset(self, processor, preset, preload=False):
 		if self.uri == preset[0]:
 			return
 		self.uri = preset[0]
@@ -256,15 +330,13 @@ class zynthian_engine_inet_radio(zynthian_engine):
 		self.command += " {}".format(self.uri)
 		self.start()
 
-
-	#----------------------------------------------------------------------------
+	# ----------------------------------------------------------------------------
 	# Controllers Management
-	#----------------------------------------------------------------------------
+	# ----------------------------------------------------------------------------
 
 	def get_controllers_dict(self, processor):
 		dict = super().get_controllers_dict(processor)
 		return dict
-
 
 	def send_controller_value(self, zctrl):
 		if zctrl.symbol == "volume":
@@ -278,17 +350,15 @@ class zynthian_engine_inet_radio(zynthian_engine):
 		elif zctrl.symbol == "wait for stream":
 			self.proc_timeout = zctrl.value
 
-
 	def get_monitors_dict(self):
-    		return self.monitors_dict
+		return self.monitors_dict
 
 	# ---------------------------------------------------------------------------
 	# Specific functions
 	# ---------------------------------------------------------------------------
 
-
 	# ---------------------------------------------------------------------------
 	# API methods
 	# ---------------------------------------------------------------------------
 
-#******************************************************************************
+# ******************************************************************************
--- zyngui/zynthian_widget_inet_radio.py
+++ zyngui/zynthian_widget_inet_radio.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-#******************************************************************************
+# ******************************************************************************
 # ZYNTHIAN PROJECT: Zynthian GUI
 # 
 # Zynthian Widget Class for "Zynthian Internet Radio"
 # 
 # Copyright (C) 2024 Brian Walton <riban@zynthian.org>
 #
-#******************************************************************************
+# ******************************************************************************
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -21,12 +21,13 @@
 #
 # For a full copy of the GNU General Public License see the LICENSE.txt file.
 # 
-#******************************************************************************
+# ******************************************************************************
 
 import tkinter
 import logging
 from zyngui import zynthian_gui_config
 from zyngui import zynthian_widget_base
+
 
 class zynthian_widget_inet_radio(zynthian_widget_base.zynthian_widget_base):
     
@@ -40,8 +41,17 @@ class zynthian_widget_inet_radio(zynthian_widget_base.zynthian_widget_base):
             bg=zynthian_gui_config.color_bg)
         self.widget_canvas.grid(sticky='news')
 
-        self.lbl_info = self.widget_canvas.create_text(
+        self.lbl_title = self.widget_canvas.create_text(
             20, 20,
+            anchor="nw",
+            font=(
+                zynthian_gui_config.font_family,
+                int(1.4 * zynthian_gui_config.font_size)
+            ),
+            fill=zynthian_gui_config.color_tx_off
+        )
+        self.lbl_info = self.widget_canvas.create_text(
+            20, 60,
             anchor="nw",
             font=(
                 zynthian_gui_config.font_family,
@@ -49,7 +59,7 @@ class zynthian_widget_inet_radio(zynthian_widget_base.zynthian_widget_base):
             ),
             fill=zynthian_gui_config.color_tx_off
         )
-        self.lbl_audio = self.widget_canvas.create_text(
+        self.lbl_bitrate = self.widget_canvas.create_text(
             20, 300,
             anchor="sw",
             font=(
@@ -58,21 +68,21 @@ class zynthian_widget_inet_radio(zynthian_widget_base.zynthian_widget_base):
             ),
             fill=zynthian_gui_config.color_tx_off,
         )
-        self.lbl_codec = self.widget_canvas.create_text(
-            20, 330,
+        self.lbl_audio = self.widget_canvas.create_text(
+            20, 340,
             anchor="sw",
             font=(
                 zynthian_gui_config.font_family,
-                zynthian_gui_config.font_size
+                int(0.7 * zynthian_gui_config.font_size)
             ),
             fill=zynthian_gui_config.color_tx_off,
         )
-        self.lbl_bitrate = self.widget_canvas.create_text(
+        self.lbl_codec = self.widget_canvas.create_text(
             20, 360,
             anchor="sw",
             font=(
                 zynthian_gui_config.font_family,
-                zynthian_gui_config.font_size
+                int(0.7 * zynthian_gui_config.font_size)
             ),
             fill=zynthian_gui_config.color_tx_off,
         )
@@ -88,14 +98,15 @@ class zynthian_widget_inet_radio(zynthian_widget_base.zynthian_widget_base):
         if event.width == self.width and event.height == self.height:
             return
         super().on_size(event)
-        self.widget_canvas.itemconfigure(self.lbl_info, width= self.width - 30)
-        self.widget_canvas.itemconfigure(self.lbl_audio, width= self.width - 30)
-        self.widget_canvas.itemconfigure(self.lbl_codec, width= self.width - 30)
-        self.widget_canvas.itemconfigure(self.lbl_bitrate, width= self.width - 30)
+        self.widget_canvas.itemconfigure(self.lbl_title, width=self.width - 30)
+        self.widget_canvas.itemconfigure(self.lbl_info, width=self.width - 30)
+        self.widget_canvas.itemconfigure(self.lbl_audio, width=self.width - 30)
+        self.widget_canvas.itemconfigure(self.lbl_codec, width=self.width - 30)
+        self.widget_canvas.itemconfigure(self.lbl_bitrate, width=self.width - 30)
         if self.height >= 400:
-            self.widget_canvas.coords(self.lbl_audio, 20, self.height - 100)
-            self.widget_canvas.coords(self.lbl_codec, 20, self.height - 40)
-            self.widget_canvas.coords(self.lbl_bitrate, 20, self.height - 10)
+            self.widget_canvas.coords(self.lbl_bitrate, 20, self.height - 60)
+            self.widget_canvas.coords(self.lbl_audio, 20, self.height - 30)
+            self.widget_canvas.coords(self.lbl_codec, 20, self.height - 10)
 
     def refresh_gui(self):
         self.refresh_count += 1
@@ -106,18 +117,20 @@ class zynthian_widget_inet_radio(zynthian_widget_base.zynthian_widget_base):
         if self.height < 400:
             # Use one field for smaller displays
             if self.monitors["info"] in ["stream unavailable", "waiting for stream..."]:
-                self.info_page = 4
+                self.info_page = 1
             if self.info_page == 1:
-                self.widget_canvas.itemconfigure(self.lbl_info, text=self.monitors["audio"])
+                self.widget_canvas.itemconfigure(self.lbl_title, text=self.monitors["title"])
             elif self.info_page == 2:
-                self.widget_canvas.itemconfigure(self.lbl_info, text=self.monitors["codec"])
+                self.widget_canvas.itemconfigure(self.lbl_title, text=self.monitors["info"])
             elif self.info_page == 3:
-                self.widget_canvas.itemconfigure(self.lbl_info, text=self.monitors["bitrate"])
+                self.widget_canvas.itemconfigure(self.lbl_title, text=self.monitors["bitrate"])
+            elif self.info_page == 4:
+                self.widget_canvas.itemconfigure(self.lbl_title, text=self.monitors["audio"])
             else:
-                self.widget_canvas.itemconfigure(self.lbl_info, text=self.monitors["info"])
-                if self.info_page > 4:
-                    self.info_page = 0
+                self.widget_canvas.itemconfigure(self.lbl_title, text=self.monitors["codec"])
+                self.info_page = 0
         else:
+            self.widget_canvas.itemconfigure(self.lbl_title, text=self.monitors["title"])
             self.widget_canvas.itemconfigure(self.lbl_info, text=self.monitors["info"])
             self.widget_canvas.itemconfigure(self.lbl_audio, text=self.monitors["audio"])
             self.widget_canvas.itemconfigure(self.lbl_codec, text=self.monitors["codec"])
