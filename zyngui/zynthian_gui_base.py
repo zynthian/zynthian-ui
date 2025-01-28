abc76ba4602a185b6719354ab904a694086c797c
riban <riban@zynthian.org>
2025-01-28 21:24:31 +0000

    Use down arrow to indicate bold press

--- zyngui/zynthian_gui_base.py
+++ zyngui/zynthian_gui_base.py
@@ -686,7 +686,7 @@ class zynthian_gui_base(tkinter.Frame):
                 else:
                     flags = "\u2665"
             if self.zyngui.bold_flag:
-                flags = "B"
+                flags = "\u21a7"
 
             self.status_canvas.itemconfig(
                 self.status_error, text=flags, fill=color)
