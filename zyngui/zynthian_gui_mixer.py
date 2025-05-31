zynthian/zynthian-ui

commit 808ba5f2b591a68fc21d5445908a936f3d3f8bb6

Remove uneeded refresh call that was causing flickering when moving slot-cursor in launcher.

zyngui/zynthian_gui_mixer.py
@@ -1659,7 +1659,8 @@ class zynthian_gui_mixer(zynthian_gui_base.zynthian_gui_base):
 
     def get_clippy_zctrl(self, zctrl_name):
         try:
-            return self.launcher_select_info['clippy'].controllers_dict[f"{zctrl_name} {info['slot'] + 1:02}"]
+            clippy_proc = self.launcher_select_info['clippy']
+            return clippy_proc.controllers_dict[f"{zctrl_name} {self.launcher_select_info['slot'] + 1:02}"]
         except:
             return None
 
@@ -1949,7 +1950,6 @@ class zynthian_gui_mixer(zynthian_gui_base.zynthian_gui_base):
         self.launcher_highlighted_slot = slot
         self.highlighted_strip.highlight_launcher(slot)
         self.set_highlighted_clip_info()
-        self.refresh_visible_strips()
 
     def end_moving_chain(self):
         if zynthian_gui_config.enable_touch_navigation:

commit f6e3a1390e2dc6826f53926e3aa95b336704f28c

Fixes scrolling in launcher which was broken by commit 808ba5f

zyngui/zynthian_gui_mixer.py
@@ -1947,13 +1947,18 @@ class zynthian_gui_mixer(zynthian_gui_base.zynthian_gui_base):
     def highlight_launcher_slot(self, slot):
         if slot < 0 or slot > self.zynseq.slots:
             return
+        refresh_strips = False
         if self.launcher_offset > slot:
             self.launcher_offset = min(slot, self.zynseq.slots - zynthian_gui_config.visible_launchers)
+            refresh_strips = True
         elif self.launcher_offset <= slot - zynthian_gui_config.visible_launchers:
             self.launcher_offset = max(0, slot - zynthian_gui_config.visible_launchers + 1)
+            refresh_strips = True
         self.launcher_highlighted_slot = slot
         self.highlighted_strip.highlight_launcher(slot)
         self.set_highlighted_clip_info()
+        if refresh_strips:
+            self.refresh_visible_strips()
 
     def end_moving_chain(self):
         if zynthian_gui_config.enable_touch_navigation: