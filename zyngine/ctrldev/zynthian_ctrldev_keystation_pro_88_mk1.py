ce4760a18fe1618b3ea52a3efa148e629475ed7b
JBrumby <brumby@web.de>
2025-09-09 22:40:05 +0200

    KEystation Pro 88 MK1 suppert for Zynthian. Also as commented Driver demo for new ctrldev structure.

---
+++ zyngine/ctrldev/zynthian_ctrldev_keystation_pro_88_mk1.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+#
+# Minimalistic Zynthian Control Device Driver for M-Audio Keystation Pro 88
+# used for Zynthina with touch screen (but without rotary encoders)
+# The device driver can controll the gui with the 4 knobs
+# 
+# The keystation pro 88 no LED, so there is no feedback possible on the device 
+# also it doesn't send key on and of messages, just program change on press,
+# so its not possible to detect long and short presses.
+#
+# Rotary Encoders are simulatet with Knobs 18, 19, 10, 11
+# 
+# this sample driver shows how easy it is to write a custom driver
+# for a specific MIDI controller
+#
+# everything that is not essential is commented out
+#
+
+import logging
+from zyngine.ctrldev.zynthian_ctrldev_base import zynthian_ctrldev_base
+
+logger = logging.getLogger('zynthian')
+
+class zynthian_ctrldev_keystation_pro_88_mk1(zynthian_ctrldev_base):
+
+   # Device identification
+   # dev_id = ["Keystation Pro 88"] # not essential
+      
+   # found no way to list the dev_ids on linux console. 
+   # They are different in Zynthian to that what I found in linux console # 
+   # with debugging it is easy to find the correct dev_ids. Found no other way. You could try 
+   # device name with " IN 1" and "IN 2" at the end
+   dev_ids = ["Keystation Pro 88 IN 1", "Keystation Pro 88 IN 2"] #  these values are ESSENTIAL for the driver to connect it to the device
+
+   # driver_name = "Keystation Pro 88 Minimal" # not essential, just for information in logs
+   
+   # driver_description = "Minimalistic Zynthian Control Device Driver für M-Audio Keystation Pro 88" 
+   # not essential. just for information in logs
+
+   # driver_version = "0.1" 
+   
+   # Helper variables for potentiometers. Hack, because ZYNPOT_ABS didn't work for me...   
+   zynpot_0 = 0
+   zynpot_1 = 0
+   zynpot_2 = 0
+   zynpot_3 = 0    
+   
+   def midi_event(self, ev):
+         """Easy MIDI event handler for Keystation Pro 88"""
+         # logger.debug(f"MIDI Event empfangen: {ev}")
+         if len(ev) > 0:
+            status= ev[0] & 0xF0 # Which midi message type (note on, note off, control change, etc.  )
+            # channel = ev[0] & 0x0F #  not usesd,
+         
+         if len(ev) == 3: # most times 3 bytes and we need 3 bytes
+               # status = ev[0] & 0xF0 # Which midi message type (note on, note off, control change, etc.  )
+               # channel = ev[0] & 0x0F #  not usesd, just for information
+               data1 = ev[1] # Note number or controller number
+               data2 = ev[2] # Note velocity or controller value
+                
+               
+               # We simulate a rotary encoder with the knobs
+               # We send the difference between the last value and the new value
+               # to the state manager ("ZYNPOT_ABS" would be easieer to use, but it doesn't work for me. It was never called in my tests)
+               # The state manager will handle the rest
+               # We have to store the last value of the knob
+               # We have 4 knobs for 4 virtual rotary encoders
+               # Knob 18 -> ZYNPOT 0
+               # Knob 19 -> ZYNPOT 1
+               # Knob 10 -> ZYNPOT 2
+               # Knob 11 -> ZYNPOT 3
+               
+               # yes I know, first time use of a knob lets jump the value from 0 to the knob value
+               # but I don't know how to get the current value of the knob at start up
+               # maybe someone can help me with that
+               
+               # 0xB0 is Control Change on MIDI Channel 1
+               # 10 is the controller number for the first knob
+               # data2 is the value of the knob (0-127)
+
+
+               if status == 0xb0 and data1 == 42:  # 42 is knob 18 in "keystations Preset 10. (Press Recall and choose 10)"
+                  # if controller sends relative values, that is: negative values for left turn and positive values for right turn,
+                  # you can use:
+                  # pot = data2
+                  # but my keystation pro 88 MK1 sends only absolute knob values from 0 to 127
+                  # so I have to calculate the difference to the last value
+                  pot = data2-self.zynpot_0 # calculates relative change of the value to use "ZYNPOT" instead of "ZYNPOT_ABS" 
+                  self.zynpot_0 = data2 # store the new value for the next change
+                  
+                  self.state_manager.send_cuia("ZYNPOT", [0, pot]) 
+                  return True # Event processed. restarts event loop
+               
+               if status == 0xb0 and data1 == 34:   # 34 is knob 19 in "Preset-Recall 10"
+                  pot = data2-self.zynpot_1
+                  self.zynpot_1 = data2
+                  self.state_manager.send_cuia("ZYNPOT", [1, pot])
+                  return True # Event processed. restarts event loop
+               
+               if ev[0] == 0xb0 and data1 == 10:   # 10 is knob 10 in "Preset-Recall 10"
+                  pot = data2-self.zynpot_2
+                  self.zynpot_2 = data2
+                  self.state_manager.send_cuia("ZYNPOT", [2, pot])
+                  return True # Event processed. restarts event loop
+               
+               if ev[0] == 0xb0 and data1 == 2:    # 2 is knob 11 in "Preset-Recall 10"
+                  pot = data2-self.zynpot_3
+                  self.zynpot_3 = data2
+                  self.state_manager.send_cuia("ZYNPOT", [3, pot])
+                  return True # Event processed. restarts event loop
+                      
+         """ if len(ev) == 3: # PC has 3 bytes.
+
+               # To use Buttons on Keystation 88 pro MK1 for "back" and "OK" is no good idea, because
+               # all Buttons are sending just a program change when pressed.
+               # You would miss them as Buttons for changing in Zynthina. But nevertheless here as example how to use it
+               
+               # status = ev[0] & 0xF0 # Which midi message type (note on, note off, control change, etc.  )
+               # channel = ev[0] & 0x0F #  not usesd, just for information
+               data1 = ev[1] # Note number or controller number    
+               
+               
+               # Buttons for "Ok" and "Back"
+               if status == 0xC0 and data1 == 0:  # Button "Back"
+                  self.state_manager.send_cuia("V5_ZYNPOT_SWITCH",  [1,"S"])
+                  # self.state_manager.send_cuia("BACK")
+                  return True # Event processed. restarts event loop
+               
+               if status == 0xC0 and data1 == 1:  # Button "OK"
+                  self.state_manager.send_cuia("V5_ZYNPOT_SWITCH",  [3,"S"])
+                  # self.state_manager.send_cuia("SELECT")
+                  return True # Event processed. restarts event loop """
+               
+         return False #  event not processed by this driver. Zynthian queue has to process it further down the row
+
+
