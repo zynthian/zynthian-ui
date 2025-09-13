a0b4d2f8a7f0ee66239ff3aff48455337fb82776
JBrumby <brumby@web.de>
2025-09-13 11:33:00 +0200

    Clean up. possibly detected a inconsistency in driver processing return value

--- zyngine/ctrldev/zynthian_ctrldev_keystation_pro_88_mk1.py
+++ zyngine/ctrldev/zynthian_ctrldev_keystation_pro_88_mk1.py
@@ -17,8 +17,13 @@
 # everything that is not essential is commented out
 #
 
+# Strange, when this driver throws an exception, the midi event is processed, as if the midi_event() sends back "False"
+# I realized this, when I had a mistake in my send_midi function (library zynseq was not importet)
+# need further exploration
+
 import logging
 from zyngine.ctrldev.zynthian_ctrldev_base import zynthian_ctrldev_base
+from zynlibs.zynseq import zynseq #  to send midi directly from this driver
 
 logger = logging.getLogger('zynthian')
 
@@ -31,7 +36,8 @@ class zynthian_ctrldev_keystation_pro_88_mk1(zynthian_ctrldev_base):
    # They are different in Zynthian to that what I found in linux console # 
    # with debugging it is easy to find the correct dev_ids. Found no other way. You could try 
    # device name with " IN 1" and "IN 2" at the end
-   dev_ids = ["Keystation Pro 88 IN 1", "Keystation Pro 88 IN 2"] #  these values are ESSENTIAL for the driver to connect it to the device
+   # dev_ids = ["Keystation Pro 88 IN 1", "Keystation Pro 88 IN 2"] #  these values are ESSENTIAL for the driver to connect it to the device
+   dev_ids = ["Keystation Pro 88 IN 1"] #  these values are ESSENTIAL for the driver to connect it to the device. Data is just at Port 1
 
    # driver_name = "Keystation Pro 88 Minimal" # not essential, just for information in logs
    
@@ -46,12 +52,32 @@ class zynthian_ctrldev_keystation_pro_88_mk1(zynthian_ctrldev_base):
    zynpot_2 = 0
    zynpot_3 = 0    
    
+   # evtype = (ev[0] >> 4) & 0x0F ->
+   EV_NOTE_OFF    = 0x8 # 3 Bytes
+   EV_NOTE_ON     = 0X9 # 3 Bytes
+   EV_AFTERTOUSCH = 0xA # 3 Bytes (polyphonic = per note)
+   EV_CC          = 0xB # 3 Bytes
+   EV_PC          = 0xC # 2 Bytes
+   EV_CHAN_PRESS  = 0xD #  2 Bytes
+   EV_PITCHBEND   = 0xE # 3 bytes ev[1] = LSB 0-127; ev[2] = MSB 0-127
+   EV_SYSTEM      = 0xF #  Systemtype = ev[0] & 0x0F
+    
+   
    def midi_event(self, ev):
          """Easy MIDI event handler for Keystation Pro 88"""
-         # logger.debug(f"MIDI Event empfangen: {ev}")
+         # self.unroute_from_chains = False # otherwise no Keyboard anymore, just a controller
+         # self.enabled = True
+         evtype = (ev[0] >> 4) & 0x0F
+         
+         if len(ev) == 3: logger.debug(f"MIDI Event empfangen: {ev} {ev[0]} {ev[1]} {ev[2]}")
+         
          if len(ev) > 0:
             status= ev[0] & 0xF0 # Which midi message type (note on, note off, control change, etc.  )
             # channel = ev[0] & 0x0F #  not usesd,
+         
+         if evtype in [self.EV_NOTE_ON, self.EV_NOTE_OFF, self.EV_AFTERTOUSCH, self.EV_PITCHBEND]:
+            return self.send_midi(ev)
+            
          
          if len(ev) == 3: # most times 3 bytes and we need 3 bytes
                # status = ev[0] & 0xF0 # Which midi message type (note on, note off, control change, etc.  )
@@ -80,7 +106,7 @@ class zynthian_ctrldev_keystation_pro_88_mk1(zynthian_ctrldev_base):
                # data2 is the value of the knob (0-127)
 
 
-               if status == 0xb0 and data1 == 42:  # 42 is knob 18 in "keystations Preset 10. (Press Recall and choose 10)"
+               if status == 0xb0 and data1 == 104:  # 42 is knob 18 in "keystations Preset 10. (Press Recall and choose 10)"
                   # if controller sends relative values, that is: negative values for left turn and positive values for right turn,
                   # you can use:
                   # pot = data2
@@ -92,19 +118,19 @@ class zynthian_ctrldev_keystation_pro_88_mk1(zynthian_ctrldev_base):
                   self.state_manager.send_cuia("ZYNPOT", [0, pot]) 
                   return True # Event processed. restarts event loop
                
-               if status == 0xb0 and data1 == 34:   # 34 is knob 19 in "Preset-Recall 10"
+               if status == 0xb0 and data1 == 105:   # 34 is knob 19 in "Preset-Recall 10"
                   pot = data2-self.zynpot_1
                   self.zynpot_1 = data2
                   self.state_manager.send_cuia("ZYNPOT", [1, pot])
                   return True # Event processed. restarts event loop
                
-               if ev[0] == 0xb0 and data1 == 10:   # 10 is knob 10 in "Preset-Recall 10"
+               if ev[0] == 0xb0 and data1 == 85:   # 10 is knob 10 in "Preset-Recall 10"
                   pot = data2-self.zynpot_2
                   self.zynpot_2 = data2
                   self.state_manager.send_cuia("ZYNPOT", [2, pot])
                   return True # Event processed. restarts event loop
                
-               if ev[0] == 0xb0 and data1 == 2:    # 2 is knob 11 in "Preset-Recall 10"
+               if ev[0] == 0xb0 and data1 == 86:    # 2 is knob 11 in "Preset-Recall 10"
                   pot = data2-self.zynpot_3
                   self.zynpot_3 = data2
                   self.state_manager.send_cuia("ZYNPOT", [3, pot])
@@ -131,7 +157,20 @@ class zynthian_ctrldev_keystation_pro_88_mk1(zynthian_ctrldev_base):
                   self.state_manager.send_cuia("V5_ZYNPOT_SWITCH",  [3,"S"])
                   # self.state_manager.send_cuia("SELECT")
                   return True # Event processed. restarts event loop """
+         
                
          return False #  event not processed by this driver. Zynthian queue has to process it further down the row
+      
+   def send_midi (self, ev):
+      chain = self.chain_manager.get_active_chain()
+            # print(chain.midi_chan)
+            # @todo: find out how to get 'last' active chain, for now: just back out.
+        
+      if chain.midi_chan is None:
+            return False
+        
+      status = (ev[0] & 0xF0) | chain.midi_chan
+      self.zynseq.libseq.sendMidiCommand(status, ev[1], ev[2])
+      return True
 
 
