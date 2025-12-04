zynthian/zynthian-ui

commit c0ecd206c9737fbca6a7b7a6647d3f4f14c1fd16

Fixes mixer learn issues

zyngui/zynthian_gui_engine.py
@@ -75,7 +75,6 @@ class zynthian_gui_engine(zynthian_gui_selector):
         super().__init__('Engine', True, False)
 
         self.chain_manager = self.zyngui.chain_manager
-        self.engine_info = self.chain_manager.engine_info
         self.engine_info_dirty = False
         self.xswipe_sens = 10
 
@@ -205,7 +204,7 @@ class zynthian_gui_engine(zynthian_gui_selector):
         if not eng_code:
             eng_code = self.list_data[self.index][0]
         try:
-            return self.engine_info[eng_code]
+            return self.chain_manager.engine_info[eng_code]
         except:
             logging.info(f"Can't get info for engine '{eng_code}'")
             return {"QUALITY": 0, "COMPLEX": 0, "DESCR": ""}
@@ -237,9 +236,13 @@ class zynthian_gui_engine(zynthian_gui_selector):
 
     def get_engines_by_cat(self):
         self.chain_manager.get_engine_info()
-        self.engine_info = self.chain_manager.engine_info
         self.proc_type = self.zyngui.modify_chain_status["type"]
         self.engines_by_cat = self.chain_manager.filtered_engines_by_cat(self.proc_type, all=self.show_all)
+        for exclude in ["MI", "MR", "MX"]:
+            try:
+                self.engines_by_cat["Other"].pop(exclude)
+            except:
+                pass
         self.engine_cats = list(self.engines_by_cat.keys())
         logging.debug(f"CATEGORIES => {self.engine_cats}")
         # self.engines_by_cat = sorted(self.engines_by_cat.items(), key=lambda kv: "!" if kv[0] is None else kv[0])
@@ -343,7 +346,7 @@ class zynthian_gui_engine(zynthian_gui_selector):
             if i is not None and self.list_data[i][0]:
                 engine = self.list_data[i][0]
                 if self.show_all:
-                    self.engine_info[engine]['ENABLED'] = not self.engine_info[engine]['ENABLED']
+                    self.chain_manager.engine_info[engine]['ENABLED'] = not self.chain_manager.engine_info[engine]['ENABLED']
                     self.engine_info_dirty = True
                     self.update_list()
                 else:

zyngine/zynthian_chain_manager.py
@@ -146,7 +146,7 @@ class zynthian_chain_manager:
         cls.engine_info = eng_info
         cls.engine_info["MI"] = {"ID":"0", "NAME":"Mixer_Channel_Strip", "TITLE": "Mixer Channel Strip", "TYPE": "Audio Effect", "CAT": "Other", "ENABLED": False, "INDEX": 0, "URL": "", "UI": "", "DESCR": "Audio mixer channel strip", "QUALITY": 5, "COMPLEX": 5, "EDIT": 0}
         cls.engine_info["MR"] = {"ID":"1", "NAME":"Mixer_Return_Strip", "TITLE": "Mixer Effect Return Strip", "TYPE": "Audio Effect", "CAT": "Other", "ENABLED": False, "INDEX": 1, "URL": "", "UI": "", "DESCR": "Audio mixer effect return strip", "QUALITY": 5, "COMPLEX": 5, "EDIT": 0}
-        cls.engine_info["MX"] = {"NAME": "Mixer", "TITLE": "ALSA Mixer", "TYPE": "MIXER", "CAT": None, "ENGINE": zynthian_engine_alsa_mixer, "ENABLED": True}
+        cls.engine_info["MX"] = {"NAME": "Alsa_Mixer", "TITLE": "ALSA Mixer", "TYPE": "Global", "CAT": None, "ENGINE": zynthian_engine_alsa_mixer, "ENABLED": False}
         # Look for an engine class for each one
         for key, info in cls.engine_info.items():
             try:
@@ -888,8 +888,8 @@ class zynthian_chain_manager:
             self.state_manager.start_busy(
                 "remove_processor", "Removing Processor", f"removing {processor.get_basepath()} from chain {chain_id}")
 
-        for param in processor.controllers_dict:
-            self.remove_midi_learn(processor, param)
+        for symbol in processor.controllers_dict:
+            self.remove_midi_learn(processor, symbol)
 
         id = None
         for i, p in self.processors.items():

zyngine/zynthian_legacy_snapshot.py
@@ -128,19 +128,20 @@ class zynthian_legacy_snapshot:
 
         if "zs3" in self.snapshot:
             for zs3 in self.snapshot["zs3"].values():
-                if "mixer" in zs3:
+                try:
+                    mixer = zs3.pop("mixer")
                     zs3.setdefault("chains", {})
                     zs3.setdefault("processors", {})
                     for chan, proc_id in mixer_map.items():
                         key = f"chan_{chan:02d}"
                         id = str(proc_id)
-                        if key in zs3["mixer"]:
-                            for param, val in zs3["mixer"][key].items():
+                        if key in mixer:
+                            for param, val in mixer[key].items():
                                 zs3["processors"].setdefault(id, {})
                                 zs3["processors"][id].setdefault("controllers", {})
                                 zs3["processors"][id]["controllers"][param]={"value":val}
-                    if "midi_learn" in zs3["mixer"]:
-                        for key, conf in zs3["mixer"]["midi_learn"].items():
+                    if "midi_learn" in mixer:
+                        for key, conf in mixer["midi_learn"].items():
                             chan, cc = key.split(",")
                             strip_id = conf[0]
                             proc_id = str(mixer_map[int(strip_id)])
@@ -148,6 +149,8 @@ class zynthian_legacy_snapshot:
                             zs3["processors"].setdefault(proc_id, {"controllers":{}})
                             zs3["processors"][proc_id]["controllers"].setdefault(symbol, {})
                             zs3["processors"][proc_id]["controllers"][symbol].setdefault("midi_cc", [None, int(chan), int(cc), 0])
+                except:
+                    pass
                 if "chains" in zs3:
                     for chain_id, cfg in zs3["chains"].items():
                         if "midi_cc" in cfg:

zyngine/zynthian_lv2.py
@@ -5,7 +5,7 @@
 #
 # zynthian LV2
 #
-# Copyright (C) 2015-2024 Fernando Moyano <jofemodo@zynthian.org>
+# Copyright (C) 2015-2025 Fernando Moyano <jofemodo@zynthian.org>
 #
 # ******************************************************************************
 #
@@ -48,6 +48,7 @@ class EngineType(Enum):
     AUDIO_EFFECT = "Audio Effect"
     AUDIO_GENERATOR = "Audio Generator"
     SPECIAL = "Special"
+    GLOBAL = "Global"
     # UNKNOWN = "Unknown"
 
 

zyngine/zynthian_processor.py
@@ -759,7 +759,6 @@ class zynthian_processor:
                 self.set_preset(state["preset_info"], force_set_engine=False)
 
         # Set controller values
-        cc_learn = []
         if "controllers" in state:
             for symbol, ctrl_state in state["controllers"].items():
                 try:
@@ -772,7 +771,7 @@ class zynthian_processor:
                     proc_name = self.get_bankpath()
                     if proc_name != "Audio Levels": # Don't show error for snapshots with different soundcard
                         logging.warning(f"Invalid controller for processor {proc_name}: {e}")
-        return cc_learn
+        return
 
     def restore_state_legacy(self, state):
         """Restore legacy states from state

zyngine/zynthian_state_manager.py
@@ -1097,6 +1097,12 @@ class zynthian_state_manager:
             if state is None:
                 return
 
+            # Remove global processor configuration
+            for proc_id, proc in self.chain_manager.processors.items():
+                if proc_id < 0:
+                    for symbol in proc.controllers_dict:
+                        self.chain_manager.remove_midi_learn(proc, symbol)
+
             # Load global MIDI configuration
             zmip_map = {}
             zmop_map = {}
@@ -1194,7 +1200,7 @@ class zynthian_state_manager:
                     self.zs3 = zs3
                 self.load_zs3(zs3["zs3-0"], autoconnect=False)
                 try:
-                    mute |= self.zs3["zs3-0"]["mixer"]["chan_16"]["mute"]
+                    mute |= state['zs3']['zs3-0']['processors'][255]['controllers']['mute']["value"]
                 except:
                     pass
 
@@ -1356,7 +1362,7 @@ class zynthian_state_manager:
             except:
                 zs3_id = "zs3-0"
 
-        restored_chains = []
+        restored_chains = [None]
         if "chains" in zs3_state:
             self.set_busy_details("restoring chains state")
             for chain_id, chain_state in zs3_state["chains"].items():

commit 689021d26e386f478d797864152573b251bf1271

Fix engine selector, broken by commit c0ecd206c9737fbca6a7b7a6647d3f4f14c1fd16.

zyngui/zynthian_gui_engine.py
@@ -306,9 +306,10 @@ class zynthian_gui_engine(zynthian_gui_selector):
             if i is not None and self.list_data[i][0]:
                 engine = self.list_data[i][0]
                 if self.show_all:
-                    self.engine_info[engine]['ENABLED'] = not self.engine_info[engine]['ENABLED']
-                    if self.engine_info[engine]['EDIT'] == 0:
-                        self.engine_info[engine]['EDIT'] = 1
+                    info = self.chain_manager.engine_info[engine]
+                    info['ENABLED'] = not info['ENABLED']
+                    if info['EDIT'] == 0:
+                        info['EDIT'] = 1
                     self.engine_info_dirty = True
                     self.update_list()
                 else: