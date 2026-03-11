b7cc0d17fdaf4e6459c79b80af5e8ca8817585b2
Niels Giesen <niels.giesen@pm.me>
2026-03-11 07:57:00 +0000

    APC Key25: Turn off screen_arranger on Bold Pad|Step
         This locked up the UI

--- zyngine/ctrldev/zynthian_ctrldev_akai_apc_key25_mk2.py
+++ zyngine/ctrldev/zynthian_ctrldev_akai_apc_key25_mk2.py
@@ -539,7 +539,7 @@ class DeviceHandler(ModeHandlerBase):
             cuia = {
                 BTN_OPT_ADMIN:   "POWER_OFF",
                 BTN_CTRL_PRESET: "PRESET_FAV",
-                BTN_PAD_STEP:    "SCREEN_ARRANGER",
+#                BTN_PAD_STEP:    "SCREEN_ARRANGER",
             }.get(btn)
             if cuia:
                 self._state_manager.send_cuia(cuia)
