3b9dc00f58b9d68d3a38cb8f6e2d93222f185467
riban <riban@zynthian.org>
2026-04-28 08:42:04 +0100

    Update TTS help. Update some info in admin view.

--- zyngui/zynthian_gui_admin.py
+++ zyngui/zynthian_gui_admin.py
@@ -174,7 +174,7 @@ class zynthian_gui_admin(zynthian_gui_selector_info):
         self.list_data.append((None, 0, "> AUDIO"))
 
         self.list_data.append((self.audio_levels, 0, "Audio Levels", ["Show audio levels view.", "meters.png"]))
-        self.list_data.append((self.show_tts, 0, "Narrator (accessibility)", ["Show the user interface narrator accessible options", None]))
+        self.list_data.append((self.show_tts, 0, "Narrator (accessibility)", ["Show the user interface narrator accessibility options", None]))
         if self.state_manager.allow_rbpi_headphones():
             if zynthian_gui_config.rbpi_headphones:
                 self.list_data.append((self.stop_rbpi_headphones, 0, "\u2612 RBPi Headphones",
@@ -186,7 +186,7 @@ class zynthian_gui_admin(zynthian_gui_selector_info):
                                         "headphones.png"]))
 
         self.list_data.append((self.hotplug_audio_menu, 0, "Hotplug USB Audio",
-                               ["Configure USB audio hotplug.\n\nWhen enabled, USB audio devices will be detected and available. This does not include any device that is already configured as the main audio device which must always remain connected.",
+                               ["Configure USB audio hotplug.\n\nWhen enabled, USB audio devices will be detected and available. This does not include any device that is already configured as the main audio device which must always remain connected and not a device used for Narrator.",
                                 "audio_options.png"]))
 
         if zynthian_gui_config.snapshot_mixer_settings:
