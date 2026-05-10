b3cb85c962302f6b1e436453547f0ffdc3af3f0a
jofemodo <fernando@zynthian.org>
2026-05-10 20:10:10 +0200

    Import CC events from SMF too.

--- zyngui/zynthian_gui_pated_base.py
+++ zyngui/zynthian_gui_pated_base.py
@@ -1046,7 +1046,7 @@ class zynthian_gui_pated_base(zynthian_gui_base):
                 evchan = evstatus & 0x0f
                 evtime = zynsmf.libsmf.getEventTime()
                 #logging.debug(f"\tEvent {event_index} => 0x{evstatus:02X}: 0x{evcode:X}, {evchan}")
-                if evcode == 0x9:
+                if evcode in (0x8, 0x9, 0xB):
                     # Count number of note-on events for each MIDI channel
                     midi_chan_info[evchan][0] += 1
                     # Look for first event time
@@ -1152,29 +1152,36 @@ class zynthian_gui_pated_base(zynthian_gui_base):
                     if start_time is not None and evtime > (start_time + ticks_in_pattern):
                         logging.debug(f"\tReached end of pattern at {evtime - start_time}!")
                         break
-                    note = zynsmf.libsmf.getEventValue1()
-                    velo = zynsmf.libsmf.getEventValue2()
-                    if evcode == 0x9 and velo:
-                        #logging.debug(f"\tNote-on at {evtime} => {note}, {velo}")
-                        # Calculate start time (first note)
+                    if evcode in (0x8, 0x9, 0xB):
+                        # Calculate start time (first event)
                         if start_time is None:
                             start_bar = evtime // ticks_per_bar
                             start_time = start_bar * self.bpb * ticks_per_beat
                             logging.debug(f"\tStart at bar {start_bar} => {start_time} ticks.")
-                        # Register Note-On
-                        note_on_info[note] = [evtime, velo]
-                    elif evcode in (0x8, 0x9):
-                        #logging.debug(f"\tNote-off at {evtime} => {note}, {velo}")
-                        ninfo = note_on_info[note]
-                        if ninfo:
-                            # Add note to pattern
-                            fstep = (ninfo[0] - start_time) / ticks_per_step
-                            step = int(fstep)
-                            duration = (evtime - ninfo[0]) / ticks_per_step
-                            offset = fstep - step
-                            self.zynseq.libseq.addNote(step, note, ninfo[1], duration, offset)
-                            logging.debug(f"\tAdd Note {note}, {ninfo[1]} => {step} + {offset}, {duration}")
-                            note_on_info[note] = None
+                        if evcode == 0xB:
+                            ccnum = zynsmf.libsmf.getEventValue1()
+                            ccval = zynsmf.libsmf.getEventValue2()
+                            step = int((evtime - start_time) / ticks_per_step)
+                            self.zynseq.libseq.addControl(step, ccnum, ccval, ccval, 1, 0)
+                        else:
+                            note = zynsmf.libsmf.getEventValue1()
+                            velo = zynsmf.libsmf.getEventValue2()
+                            if evcode == 0x9 and velo:
+                                #logging.debug(f"\tNote-on at {evtime} => {note}, {velo}")
+                                # Register Note-On
+                                note_on_info[note] = [evtime, velo]
+                            else:
+                                #logging.debug(f"\tNote-off at {evtime} => {note}, {velo}")
+                                ninfo = note_on_info[note]
+                                if ninfo:
+                                    # Add note to pattern
+                                    fstep = (ninfo[0] - start_time) / ticks_per_step
+                                    step = int(fstep)
+                                    duration = (evtime - ninfo[0]) / ticks_per_step
+                                    offset = fstep - step
+                                    self.zynseq.libseq.addNote(step, note, ninfo[1], duration, offset)
+                                    logging.debug(f"\tAdd Note {note}, {ninfo[1]} => {step} + {offset}, {duration}")
+                                    note_on_info[note] = None
         # TODO Off pending notes?
         # Save state for undo/redo
         self.save_pattern_snapshot(now=True, force=True)
