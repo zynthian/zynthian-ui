f57c0524f8e0f1de3bc5667e37e49a18daa21cf4
riban <riban@zynthian.org>
2026-05-15 11:28:38 +0100

    Fix help TTS header sentence being seperate from the header text.

--- zyngui/zynthian_gui_help.py
+++ zyngui/zynthian_gui_help.py
@@ -398,14 +398,14 @@ class zynthian_gui_help(HtmlFrame):
                                 self.tts_knobs.append(f"{encoder_actions[i]} {action.get_text()}. ")
                     knob_action_container.decompose()
 
-                # Ensure brief pause after each header and paragraph
+                # Ensure brief pause after each paragraph
                 for tag in self.soup.find_all(["p", "br"]):
                     tag.insert_after(". ")
                 for tag in self.soup.find_all(class_="no_tts"):
                     tag.decompose()
                 for i in range(1, 7):
                     for tag in self.soup.find_all(f"h{i}"):
-                        tag.insert_before(f". Header {i}. ")
+                        tag.insert_before(f". Header {i}: ")
                         tag.insert_after(". ")
                 for tag in self.soup.find_all("ul"):
                     tag.insert_before(". List. ")
