zynthian/zynthian-ui

commit 6bf4035d0eaf1eb57f4a9dcc4cb6b07b63771bd4

Fix TTS pronounciation when enabling parameter editor. Fix TTS announcment of parameter editor values. Use processor name for control view help. Fix wrong logic in filter widget show function. Add tempo help.

zyngui/zynthian_widget_filter.py
@@ -151,7 +151,7 @@ class zynthian_widget_filter(zynthian_widget_base.zynthian_widget_base):
         # 1. Normalize for math consistency across engines
         norm_cutoff = (self.cutoff_param.value - self.cutoff_param.value_min) / self.cutoff_param.value_range if self.cutoff_param else 0.5
         norm_res = (self.resonance_param.value - self.resonance_param.value_min) / self.resonance_param.value_range if self.resonance_param else 0.0
-        if force or [norm_cutoff, norm_res] == self.last_values and not self.is_dragging:
+        if not force or [norm_cutoff, norm_res] == self.last_values and not self.is_dragging:
             return
 
         # 2. Precalculate some geometric values

help/core/tempo.html
@@ -1,0 +1,24 @@
+<html>
+ <head>
+  <link rel="stylesheet" href="../style.css">
+ </head>
+
+ <body>
+ <h1>Tempo View</h1>
+    <p>The tempo view provides control of zynthian's timebase used for sequencing, arpreggiators, etc.</p>
+    <p>The BPM control sets the internal tempo between the values of 10 and 420 BPM. Push and rotate to adjust in 0.1 BPM steps. This control is available if using internal clock.</p>
+    <p> If external clock is used, the BPM control changes to be a clock divider. By default it expects a 24 PPQN input which is what MIDI clock uses but may be adjusted to support other external clock rates in the range 1 to 48 PPQN. The internal clock runs at 1920 PPQN and uses a phase locked loop to derive this high resolution clock from the slow input clock. To use an external clock, select "MIDI Clock Source" for a MIDI input in the <a href="chain_midi_input.html">MIDI Input</a> view.</p>
+    <p>The Metronome Mode control configures the zynthian's metronome. It has these values:
+      <ul>
+         <li>OFF: Metronome is disabled.</li>
+         <li>AUTO: Metronome will sound when there is a sequence playing and stop at the end of the bar after the last sequence stops.</li>
+         <li>ON: Metronome plays continuously.</li>
+         <li>INTRO: Metronome plays until a sequence starts. You may use this as a count-in when you don't want or need the metronome to play during performance.</li>
+         <li>NO ACCENT: Metronome plays continulaly but with without an accent on the first beat of each bar. This acts more like a traditional metronome used purely for tempo and not timebase.</li>
+         <li>SILENT: Metronome runs but does not make any sound. This can be useful to cause the transport to run when there are no sequences running. Some plugins may benefit from this.</li>
+      </ul>
+      There is a control to adjust the metronome volume level.</p>
+    <p>There is a large display of the tempo which may be tapped to set the tempo. Similarly, the tempo button may be tapped to set tempo.</p>
+    <div class="note-block">Note: The metronome feeds the main chain, via its auxiliary mix feed which allows individual control of level, balance, etc. The global audio player shares this aux mix.</div>
+ </body>
+</html>

zyngui/zynthian_gui_base.py
@@ -626,7 +626,7 @@ class zynthian_gui_base(tkinter.Frame):
 
         self.label_select_path.config(bg=zynthian_gui_config.color_panel_tx, fg=zynthian_gui_config.color_header_bg)
         if self.zyngui.tts:
-            self.zyngui.tts.announce("Enabled parameter editor")
+            self.zyngui.tts.announce("Enabled param editor")
         self.update_param_editor(True)
         self.update_layout()
 
@@ -643,21 +643,23 @@ class zynthian_gui_base(tkinter.Frame):
         except:
             pass
         if self.zyngui.tts:
-            self.zyngui.tts.announce("Disabled parameter editor")
+            self.zyngui.tts.announce("Disabled param editor")
 
     # Function to display label in parameter editor
     def update_param_editor(self, first_show=False):
         if self.param_editor_zctrl:
             if self.param_editor_zctrl.labels:
-                text = f"{self.param_editor_zctrl.name}: {self.param_editor_zctrl.get_value2label()}"
+                value = self.param_editor_zctrl.get_value2label()
+                text = f"{self.param_editor_zctrl.name}: {value}"
             else:
-                text = self.format_print.format(self.param_editor_zctrl.name, self.param_editor_zctrl.value)
+                value = self.param_editor_zctrl.value
+                text = self.format_print.format(self.param_editor_zctrl.name, value)
             self.select_path.set(text)
             if self.zyngui.tts:
                 if first_show:
                     self.zyngui.tts.announce(text, False, False, False)
                 else:
-                    self.zyngui.tts.announce(str(self.param_editor_zctrl.value))
+                    self.zyngui.tts.announce(str(value))
 
     # --------------------------------------------------------------------------
     # MIDI learning

zyngui/zynthian_gui_control.py
@@ -946,6 +946,12 @@ class zynthian_gui_control(zynthian_gui_selector):
         else:
             self.select_path.set(self.chain_manager.get_active_chain().get_title())
 
+    def get_help_fpath(self):
+        try:
+            return self.zyngui.get_current_processor().name.lower() + ".html"
+        except:
+            return super().get_help_fpath()
+
     # --------------------------------------------------------------------------
     # Narrator TTS
     # --------------------------------------------------------------------------

commit 96025f4de0f90295e0f0652cbaa08e1b728e6fd7

Fix filter widget refresh, broke by 6bf4035d.

zyngui/zynthian_widget_filter.py
@@ -21,8 +21,10 @@
 # ******************************************************************************
 
 
-import tkinter
 import math
+import tkinter
+import logging
+
 from zyngui import zynthian_gui_config
 from zyngui import zynthian_widget_base
 
@@ -151,7 +153,7 @@ class zynthian_widget_filter(zynthian_widget_base.zynthian_widget_base):
         # 1. Normalize for math consistency across engines
         norm_cutoff = (self.cutoff_param.value - self.cutoff_param.value_min) / self.cutoff_param.value_range if self.cutoff_param else 0.5
         norm_res = (self.resonance_param.value - self.resonance_param.value_min) / self.resonance_param.value_range if self.resonance_param else 0.0
-        if not force or [norm_cutoff, norm_res] == self.last_values and not self.is_dragging:
+        if not (force and self.is_dragging) and [norm_cutoff, norm_res] == self.last_values:
             return
 
         # 2. Precalculate some geometric values