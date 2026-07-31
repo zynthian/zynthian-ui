282a1f8a5bee5bb9b5704d8544a3c105e49cf841
jofemodo <fernando@zynthian.org>
2026-07-31 17:30:54 +0200

    Reduce line split excess in source code.

--- zyngui/zynthian_gui_wifi.py
+++ zyngui/zynthian_gui_wifi.py
@@ -54,8 +54,7 @@ class zynthian_gui_wifi(zynthian_gui_selector_info):
     def build_view(self):
         if not self.scan_thread:
             self.wifi_scan = True
-            self.scan_thread = Thread(
-                target=self.scan_wifi_task, name="wifi_scan")
+            self.scan_thread = Thread(target=self.scan_wifi_task, name="wifi_scan")
             self.scan_thread.start()
         return super().build_view()
 
@@ -75,28 +74,22 @@ class zynthian_gui_wifi(zynthian_gui_selector_info):
     def fill_list(self):
         wifi_status_code = zynconf.get_nwdev_status_code("wlan0")
         if wifi_status_code == 20:
-            self.list_data = [
-                (self.enable_wifi, None, "\u2610 Wi-Fi is disabled", False, False, ["Toggle Wi-Fi enable.", None])]
+            self.list_data = [(self.enable_wifi, None, "\u2610 Wi-Fi is disabled", False, False, ["Toggle Wi-Fi enable.", None])]
         elif wifi_status_code in (30, 50, 100):
-            self.list_data = [
-                (self.disable_wifi, None, "\u2612 Wi-Fi is enabled", False, False, ["Toggle Wi-Fi enable.", None])]
+            self.list_data = [(self.disable_wifi, None, "\u2612 Wi-Fi is enabled", False, False, ["Toggle Wi-Fi enable.", None])]
             if not self.wifi_data:
-                self.list_data.append(
-                    (None, None, "Scanning Wi-Fi Networks...", False, False))
+                self.list_data.append((None, None, "Scanning Wi-Fi Networks...", False, False))
             else:
-                self.list_data.append(
-                    (None, None, "Wi-Fi Networks", False, False))
+                self.list_data.append((None, None, "Wi-Fi Networks", False, False))
                 self.list_data += self.wifi_data
         else:
-            self.list_data = [
-                (None, None, f"Wi-Fi doesn't work! ({wifi_status_code})", False, False)]
+            self.list_data = [(None, None, f"Wi-Fi doesn't work! ({wifi_status_code})", False, False)]
         super().fill_list()
 
     def enable_wifi(self):
         self.state_manager.start_busy("wifi_enable", f"Enabling Wi-Fi")
         try:
-            check_output(["nmcli", "--terse", "radio",
-                         "wifi", "on"], encoding='utf-8')
+            check_output(["nmcli", "--terse", "radio", "wifi", "on"], encoding='utf-8')
             res = True
             sleep(2)
         except Exception as e:
@@ -109,8 +102,7 @@ class zynthian_gui_wifi(zynthian_gui_selector_info):
     def disable_wifi(self):
         self.state_manager.start_busy("wifi_disable", f"Disabling Wi-Fi")
         try:
-            check_output(["nmcli", "--terse", "radio",
-                         "wifi", "off"], encoding='utf-8')
+            check_output(["nmcli", "--terse", "radio", "wifi", "off"], encoding='utf-8')
             res = True
         except Exception as e:
             res = False
@@ -120,11 +112,9 @@ class zynthian_gui_wifi(zynthian_gui_selector_info):
         return res
 
     def enable_wifi_nw(self, name):
-        self.state_manager.start_busy(
-            "wifi_enable_nw", f"Connecting to {name}")
+        self.state_manager.start_busy("wifi_enable_nw", f"Connecting to {name}")
         try:
-            output = check_output(
-                ["nmcli", "--terse", "con", "up", name], encoding='utf-8')
+            output = check_output(["nmcli", "--terse", "con", "up", name], encoding='utf-8')
             if "Connection successfully activated" in output:
                 res = True
                 self.state_manager.set_busy_success(f"Connected to {name}")
@@ -136,8 +126,7 @@ class zynthian_gui_wifi(zynthian_gui_selector_info):
                 sleep(2)
         except Exception as e:
             res = False
-            self.state_manager.set_busy_error(
-                f"Can't enable network {name}", str(e))
+            self.state_manager.set_busy_error(f"Can't enable network {name}", str(e))
             sleep(2)
         self.state_manager.end_busy("wifi_enable_nw")
         if not res:
@@ -145,15 +134,12 @@ class zynthian_gui_wifi(zynthian_gui_selector_info):
         return res
 
     def disable_wifi_nw(self, name):
-        self.state_manager.start_busy(
-            "wifi_disable_nw", f"Disconnecting from {name}")
+        self.state_manager.start_busy("wifi_disable_nw", f"Disconnecting from {name}")
         try:
-            output = check_output(
-                ["nmcli", "--terse", "con", "down", name], encoding='utf-8')
+            output = check_output(["nmcli", "--terse", "con", "down", name], encoding='utf-8')
             if "successfully deactivated" in output:
                 res = True
-                self.state_manager.set_busy_success(
-                    f"Disconnected from {name}")
+                self.state_manager.set_busy_success(f"Disconnected from {name}")
                 sleep(2)
             else:
                 res = False
@@ -162,8 +148,7 @@ class zynthian_gui_wifi(zynthian_gui_selector_info):
                 sleep(3)
         except Exception as e:
             res = False
-            self.state_manager.set_busy_error(
-                f"Can't disable network {name}", str(e))
+            self.state_manager.set_busy_error(f"Can't disable network {name}", str(e))
             sleep(3)
         self.state_manager.end_busy("wifi_disable_nw")
         return res
@@ -173,15 +158,12 @@ class zynthian_gui_wifi(zynthian_gui_selector_info):
         self.zyngui.show_keyboard(self.configure_wifi_nw_cb, "")
 
     def configure_wifi_nw_cb(self, passwd):
-        self.state_manager.start_busy(
-            "wifi_config_nw", f"Connecting to {self.config_wifi_name}")
+        self.state_manager.start_busy("wifi_config_nw", f"Connecting to {self.config_wifi_name}")
         try:
-            output = check_output(["nmcli", "dev", "wifi", "connect",
-                                  self.config_wifi_name, "password", passwd], encoding='utf-8')
+            output = check_output(["nmcli", "dev", "wifi", "connect", self.config_wifi_name, "password", passwd], encoding='utf-8')
             if "successfully activated" in output:
                 res = True
-                self.state_manager.set_busy_success(
-                    f"Connected to {self.config_wifi_name}")
+                self.state_manager.set_busy_success(f"Connected to {self.config_wifi_name}")
                 self.config_wifi_name = None
                 sleep(2)
             else:
@@ -192,8 +174,7 @@ class zynthian_gui_wifi(zynthian_gui_selector_info):
         except Exception as e:
             res = False
             logging.debug(f"{e}")
-            self.state_manager.set_busy_error(
-                f"Can't configure network {self.config_wifi_name}", str(e))
+            self.state_manager.set_busy_error(f"Can't configure network {self.config_wifi_name}", str(e))
             sleep(2)
         self.state_manager.end_busy("wifi_config_nw")
         if not res:
@@ -202,8 +183,7 @@ class zynthian_gui_wifi(zynthian_gui_selector_info):
 
     def delete_wifi_nw(self, name):
         try:
-            output = check_output(
-                ["nmcli", "con", "del", name], encoding='utf-8')
+            output = check_output(["nmcli", "con", "del", name], encoding='utf-8')
             res = True
         except Exception as e:
             res = False
