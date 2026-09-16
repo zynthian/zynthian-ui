d00773f43e9fb64d4ed90d2d075a2141102b8ee4
jofemodo <fernando@zynthian.org>
2026-09-16 11:08:27 +0200

    Move SFZ opcode detection functions. Disable code while it's not used.

--- zyngine/zynthian_engine_sfizz.py
+++ zyngine/zynthian_engine_sfizz.py
@@ -32,8 +32,6 @@ import yaml
 from pathlib import Path
 
 from zyngine.zynthian_engine_sfz import zynthian_engine_sfz
-SFIZZ_YAML_URL = "https://raw.githubusercontent.com/sfztools/sfztools.github.io/refs/heads/master/data/sfizz/support.yml"
-SFIZZ_SUPPORTED = "/zynthian/config/sfizz_supported.yml"
 
 # ------------------------------------------------------------------------------
 # Sfizz Engine Class
@@ -291,99 +289,6 @@ class zynthian_engine_sfizz(zynthian_engine_sfz):
     @classmethod
     def zynapi_martifact_formats(cls):
         return "sfz"
-
-    @classmethod
-    def get_supported_opcode_patterns(cls, download=False):
-        """ Get list of regex patterns describing sfizz supported sfz codes
-        Args:
-            download: True to download latest support yaml file
-        Returns: List of regex patterns describing supported opcodes
-        """
-
-        patterns = []
-        if download and os.system(f"wget -N -O {SFIZZ_SUPPORTED} {SFIZZ_YAML_URL} > /dev/null 2>&1") != 0:
-            return patterns
-
-        try:
-            with open(SFIZZ_SUPPORTED, "r") as f:
-                data = yaml.safe_load(f)
-        except:
-            return patterns
-        if not data:
-            return patterns
-
-        opcodes = data.get("opcodes", [])
-        for cat in data['categories']:
-            opcodes += cat.get('opcodes', [])
-        for op in opcodes:
-            if "support" in op:
-                continue
-            name = op.get("name")
-            if not name:
-                continue
-            escaped = re.escape(name)
-            escaped = re.sub(r"[A-Z]", r"\\d+", escaped)
-            patterns.append(re.compile(f"^{escaped}$"))
-        return patterns
-    
-    @classmethod
-    def get_used_codes(cls, filename):
-        """Extract headers and opcodes from a .sfz file
-        Args:
-            filename: Full path of sfz file
-        Returns: Dict of headers and opcodes used by sfz
-        """
-
-        used = {"headers": set(), "opcodes": set()}
-        with open(filename, "r", encoding="utf-8") as f:
-            for line in f:
-                line = re.sub(r"//.*", "", line)
-                line = re.sub(r"/\*.*?\*/", "", line)
-                headers = re.findall(r"<[^>]+>", line)
-                used["headers"].update(headers)
-                matches = re.findall(r"([A-Za-z0-9_]+)\s*=", line)
-                used["opcodes"].update(matches)
-        return used
-
-    @classmethod
-    def get_all_sfz(cls, path):
-        """ Get a list of all sfz files within a directory branch (recurssive)
-        Args:
-            path: Path of directory to search
-        Returns: List of sfz files
-        """
-
-        return list(Path(path).rglob("*.sfz"))
-
-    @classmethod
-    def is_opcode_supported(cls, opcode, patterns):
-        """ Check if opcode matches any supported pattern
-        Args:
-            opcode: Opcode to validate
-            patterns: List of regexp patterns used to validate
-        Returns: True if valid opcode
-        """
-
-        for pattern in patterns:
-            if pattern.match(opcode):
-                return True
-        return False
-
-    @classmethod
-    def get_unsupported_opcodes(cls, filename, patterns):
-        """ Get list of unsupported opcodes used by sfz
-        Args:
-            filename: Full path of sfz file
-            patterns: List of regex patterns describing supported opcodes (see get_supported_opcode_patterns)
-        Returns: List of unsupported opcodes
-        """
-
-        unsupported = []
-        for opcode in zynthian_engine_sfizz.get_used_codes(filename)["opcodes"]:
-            if not zynthian_engine_sfizz.is_opcode_supported(opcode, patterns):
-                unsupported.append(opcode)
-        return unsupported
-
 
 
 # ******************************************************************************
